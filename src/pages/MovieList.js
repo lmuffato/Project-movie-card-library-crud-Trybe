02d60397937d8957d3c3fc2c13f348522d4ab440
Felipe Flores <felipefloresvendas@gmail.com>
2021-05-07 19:32:24 -0300

    Requirement 2 Done

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -8,19 +8,36 @@ class MovieList extends Component {
   constructor() {
     super();
 
+    this.getAllMovies = this.getAllMovies.bind(this);
+
     this.state = {
       movies: [],
+      loading: false,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.getAllMovies();
+  }
 
+  getAllMovies() {
+    this.setState({ loading: true }, async () => {
+      const allMovies = await movieAPI.getMovies();
+      this.setState((previousState) => ({
+        movies: [...previousState.movies, ...allMovies],
+        loading: false,
+      }));
+    });
+  }
+
+  render() {
+    const { movies, loading } = this.state;
     // Render Loading here if the request is still happening
-    <Loading />
+
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading /> : movies
+          .map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

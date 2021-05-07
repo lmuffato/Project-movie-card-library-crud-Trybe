aed2e4c63e894837f317de3ff83de34d6e835ea9
Gabriel Ferreira <glucas709@gmail.com>
2021-05-07 17:43:35 -0300

    Requisito 01

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component {
+  render() {
+    console.log(window.location.pathname);
+    return (
+      <Router>
+        <div>Movie Card Library CRUD</div>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route component={ NotFound } />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;

eb1147450c9a39dfe92e6bf1939184a261a063d4
Orlando Flores <orlando_flores@id.uff.br>
2021-05-07 19:17:38 -0300

    corrigindo lint

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';

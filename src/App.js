98e34be83a26d749db4c9c86fe9438ecce1551e2
Mateus Alencar <mtauws@gmail.com>
2021-05-07 18:13:32 -0300

    Requisito 1: Renderize BrowserRouter no App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { MovieList, NewMovie, EditMovie, MovieDetails, NotFound } from './pages';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Route exact path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route component={ NotFound } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
     </Router>
   );
 }
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -4,10 +4,12 @@ import MovieForm from '../components/MovieForm';
 // import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
+  /*
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  */
 
   /*
   handleSubmit(newMovie) {

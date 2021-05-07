247ab5e5cf093895e4d8831a74a075ba26de3756
Gabriel Ferreira <glucas709@gmail.com>
2021-05-07 20:36:19 -0300

    Requisito 03

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Rating from './Rating';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, storyline, rating, imagePath } = movie;
+    const { title, storyline, imagePath, id } = movie;
     return (
       <div className="movie-card" id="movie-card" data-testid="movie-card">
         <img alt="Movie Cover" className="movie-card-image" src={ imagePath } />
@@ -14,7 +14,7 @@ class MovieCard extends React.Component {
           {/* <h5 className="movie-card-subtitle">{subtitle}</h5> */}
           <p className="movie-card-storyline">{storyline}</p>
         </div>
-        <Rating rating={ rating } />
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
@@ -28,6 +28,7 @@ MovieCard.propTypes = {
     rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
     imagePath: PropTypes.string,
     bookmarked: PropTypes.bool,
+    id: PropTypes.number,
   }).isRequired,
 };
 

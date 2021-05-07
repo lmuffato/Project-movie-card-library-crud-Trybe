a18976228dc8359734005fdb45fc1844ed7f34c8
Rodrigo Rech <rodrigorech92@gmail.com>
2021-05-07 17:55:47 -0300

    BrowserRouter Component rederized at App - Some lint errors will fix while doing the other requeriments

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        {/* // https://ultimatecourses.com/blog/react-router-not-found-component  aprendi sobre rota NotFound */}
+        <Route component={ NotFound } />
+        <div>Movie Card Library CRUD</div>
+      </Switch>
     </Router>
   );
 }

a4b70b1bc01800e0b3c864d7ba1b4012694d2b48
Felipe Muller <felipeavmuller@gmail.com>
2021-05-07 16:38:03 -0300

    Erros de lint para iniciar o projeto

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 class MovieForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { ...props.movie };
+    // this.state = { ...props.movie };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit() {
-    const { onSubmit } = this.props;
-    onSubmit(this.state);
-  }
+  // handleSubmit() {
+  //   const { onSubmit } = this.props;
+  //   onSubmit(this.state);
+  // }
 
   updateMovie(field, newValue) {
     this.setState({ [field]: newValue });

2b3a3d9a5b00b112159daf8eba3431983919b5de
Giovanni <giovannitadeu08@gmail.com>
2021-05-07 19:40:21 -0300

    [Requisitos 4 e 7] - Feitos

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,85 @@
 import React, { Component } from 'react';
-
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+import MovieList from './MovieList';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      loading: true,
+      shouldRedirect: false,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    const { match } = this.props;
+    const { id } = match.params;
+
+    const movie = await movieAPI.getMovie(id);
+
+    this.setState({
+      movie,
+      loading: false,
+    });
+  }
+
+  async removeMovie(id) {
+    const request = await movieAPI.deleteMovie(id);
+
+    return (request.status === 'OK') && this.setState({ shouldRedirect: true });
+  }
+
+  checkImageURL(image) {
+    if (!image.includes('http')) return `../${image}`;
+    return image;
+  }
+
+  structureDetails({ title, storyline, imagePath, genre, rating, subtitle }) {
+    const { match } = this.props;
+    const { id } = match.params;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <img alt="Movie Cover" src={ this.checkImageURL(imagePath) } />
+        <h1>{ title }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
-      </div>
-    );
+        <div className="links">
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          <Link to="/" onClick={ () => this.removeMovie(id) }>DELETAR</Link>
+          <Link to="/">VOLTAR</Link>
+        </div>
+      </div>);
+  }
+
+  render() {
+    const { movie, shouldRedirect, loading } = this.state;
+
+    if (shouldRedirect) {
+      return <Redirect to="/" component={ MovieList } />;
+    }
+
+    return (loading) ? <Loading /> : this.structureDetails(movie);
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;

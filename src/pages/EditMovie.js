694dcb2bd3ac3b19468c86bc42989a8c79502358
Giovanni <giovannitadeu08@gmail.com>
2021-05-07 20:06:46 -0300

    [Requisitos 5 e 6] - Feitos

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,34 +1,65 @@
 import React, { Component } from 'react';
-
-import { MovieForm } from '../components';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import MovieList from './MovieList';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      loading: true,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    this.fetchMovie();
   }
 
-  render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+  async handleSubmit(updatedMovie) {
+    const request = await movieAPI.updateMovie(updatedMovie);
+    return (request === 'OK') && this.setState({ shouldRedirect: true });
+  }
 
-    if (status === 'loading') {
-      // render Loading
-    }
+  async fetchMovie() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const movie = await movieAPI.getMovie(id);
 
+    this.setState({
+      movie,
+      loading: false,
+    });
+  }
+
+  structureEdit(movie) {
     return (
       <div data-testid="edit-movie">
         <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
+
+  render() {
+    const { loading, shouldRedirect, movie } = this.state;
+    if (shouldRedirect) {
+      return <Redirect to="/" component={ MovieList } />;
+    }
+
+    return (loading) ? <Loading /> : this.structureEdit(movie);
+  }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;

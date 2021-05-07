60ecb0e1a31dde7071a2f8c0f9368d5570fcb876
Edrei <edreimateus@gmail.com>
2021-05-07 16:36:41 -0300

    initial commit

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import { MovieForm } from '../components';
-import * as movieAPI from '../services/movieAPI';
+// import { MovieForm } from '../components';
+// import * as movieAPI from '../services/movieAPI';
 
-class EditMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+// class EditMovie extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  handleSubmit(updatedMovie) {
-  }
+//   handleSubmit(updatedMovie) {
+//   }
 
-  render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+//   render() {
+//     const { status, shouldRedirect, movie } = this.state;
+//     if (shouldRedirect) {
+//       // Redirect
+//     }
 
-    if (status === 'loading') {
-      // render Loading
-    }
+//     if (status === 'loading') {
+//       // render Loading
+//     }
 
-    return (
-      <div data-testid="edit-movie">
-        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
-      </div>
-    );
-  }
-}
+//     return (
+//       <div data-testid="edit-movie">
+//         <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+//       </div>
+//     );
+//   }
+// }
 
-export default EditMovie;
+// export default EditMovie;

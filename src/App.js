fbc946d1856c006856eb36eb31d5dbe643f4c544
Willian Mendes <willianmendesbr@gmail.com>
2021-05-07 16:09:47 -0300

    requisito 1 - rotas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { EditMovie, MovieDetails, MovieList, NewMovie, NotFound } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route exact path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route component={ NotFound } />
+    </BrowserRouter>
   );
 }
 

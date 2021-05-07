501345aa34b7ae6b734c520fe199b24b9c9155c3
Mateus Alencar <mtauws@gmail.com>
2021-05-07 18:26:20 -0300

    Requisito 2: Fazer uma req para mostrar os filmes.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +10,29 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.showFilms();
+  }
 
-    // Render Loading here if the request is still happening
+  async showFilms() {
+    const response = await movieAPI.getMovies();
+    this.setState({ movies: response, loading: false });
+  }
+
+  render() {
+    const { movies, loading } = this.state;
 
     return (
+      // Source: https://github.com/tryber/sd-08-project-movie-cards-library-crud/tree/ailsonjr-project-movie-card-library-crud
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {
+          loading ? <Loading /> : movies
+            .map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
       </div>
     );
   }

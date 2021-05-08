e006f2d1c821b4fa13b6a6568ccb36a5b1c425af
Felipe Flores <felipefloresvendas@gmail.com>
2021-05-08 19:58:23 -0300

    Requirement 6 Done

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
@@ -38,6 +39,7 @@ class MovieList extends Component {
       <div data-testid="movie-list">
         {loading ? <Loading /> : movies
           .map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }

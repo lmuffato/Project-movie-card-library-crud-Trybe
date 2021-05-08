825de35594bc64968cc4841907fca1b10faa6b07
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-08 19:15:24 -0300

    requisito 3, falta reparar lint

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, id, storyline } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <p>{ title }</p>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  id: PropTypes.number,
+  story: PropTypes.string,
+}.isRequired;
 
 export default MovieCard;

78a546b72222b1efdc6af387b813e31dc39b2142
Wanderson Sales <wandersonsales.dev@gmail.com>
2021-05-07 21:07:23 -0300

    :rocket: Adicionando um pouco de layout

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
+import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/MovieDetails.css';
 
 class MovieDetails extends Component {
   constructor() {
@@ -48,15 +52,31 @@ class MovieDetails extends Component {
     const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
-      <div data-testid="movie-details">
-        <h2>{ title }</h2>
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
-        <Link to="/">VOLTAR</Link>
-        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+      <div data-testid="movie-details" className="movieDetails">
+        <Card>
+          <Card.Img src={ `http://localhost:3000/${imagePath}` } alt="Movie Cover" />
+          <Card.Body>
+            <Card.Title>
+              { title }
+            </Card.Title>
+            <Card.Subtitle className="storyline">
+              { subtitle }
+            </Card.Subtitle>
+            <Card.Subtitle className="storyline">
+              { storyline }
+            </Card.Subtitle>
+            <Card.Subtitle>
+              { genre }
+            </Card.Subtitle>
+            <Card.Text>
+              { rating }
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            <Link to="/">VOLTAR</Link>
+            <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          </Card.Footer>
+        </Card>
       </div>
     );
   }

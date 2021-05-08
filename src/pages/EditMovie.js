104abc772c8440b15e6727e0471c52195f243fe0
Marilia Cegalla Aldrighi <maricegalla@gmail.com>
2021-05-08 10:14:09 -0300

    Requisito 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,27 +1,51 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loading: true,
+      redirect: false,
+      movie: [],
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
-    console.log(updatedMovie);
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState({ redirect: true });
+  }
+
+  async fetchMovie() {
+    const { match } = this.props;
+    const { params } = match;
+    const response = await movieAPI.getMovie(params.id);
+    this.setState({
+      movie: response,
+      loading: false,
+    });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+    const { loading, redirect, movie } = this.state;
 
-    if (status === 'loading') {
-      // render Loading
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
+    const loadingElement = <Loading />;
+
+    if (loading) {
+      return loadingElement;
     }
 
     return (
@@ -31,5 +55,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;

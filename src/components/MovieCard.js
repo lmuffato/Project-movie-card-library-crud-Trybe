225e6d9666c815e00af71be22b84c4389e0cdc9c
Felipe Flores <felipefloresvendas@gmail.com>
2021-05-07 21:10:44 -0300

    Requirement 3 Done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import MovieDetails from '../pages/MovieDetails';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h3>{ title }</h3>
+        <p>{ storyline }</p>
+        {/* <Link to={ id } component={ MovieDetails } /> */}
+        {/* <Link to={ MovieDetails } /> */}
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  title: PropTypes.string,
+  storyline: PropTypes.string,
+}.isRequired;
 
 export default MovieCard;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,6 +4,32 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+
+  // constructor() {
+  //   super();
+
+  //   this.getAllMovies = this.getAllMovies.bind(this);
+
+  //   this.state = {
+  //     movies: [],
+  //     loading: false,
+  //   };
+  // }
+
+  // componentDidMount() {
+  //   this.getAllMovies();
+  // }
+
+  // getAllMovies() {
+  //   this.setState({ loading: true }, async () => {
+  //     const allMovies = await movieAPI.getMovies();
+  //     this.setState((previousState) => ({
+  //       movies: [...previousState.movies, ...allMovies],
+  //       loading: false,
+  //     }));
+  //   });
+  // }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;

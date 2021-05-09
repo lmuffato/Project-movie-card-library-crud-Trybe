944e341524f4b3575da651187843352cda044f6b
Marcelo Maurício <mmauricio.design@gmail.com>
2021-05-09 13:25:20 -0300

    Requisito 5 - Realiza uma requisicao de atualizacao de filmes, onde possibilita editar os filmes que vem da API

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 
-import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import { MovieForm, Loading } from '../components';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
-    // this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      status: 'loading',
+      shouldRedirect: false,
+      movie: [],
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.getMovieApi = this.getMovieApi.bind(this);
   }
 
-  // handleSubmit(updatedMovie) {
-  // }
+  componentDidMount() {
+    this.getMovieApi();
+  }
+
+  async handleSubmit(updatedMovie) {
+    await movieAPI.updateMovie(updatedMovie);
+    this.setState(() => ({ shouldRedirect: true }));
+  }
+
+  async getMovieApi() {
+    const { match } = this.props;
+    const idNumberPage = match.params.id;
+
+    const movieGet = await movieAPI.getMovie(idNumberPage);
+    this.setState(() => ({ movie: movieGet, status: 'ok' }));
+  }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
       // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
       // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +55,9 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.object,
+}.isRequired;
 
 export default EditMovie;

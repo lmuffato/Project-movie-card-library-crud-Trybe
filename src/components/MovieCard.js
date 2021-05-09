a005a50cc61a8383c12e8224f7e0b8138a3e6100
MaxRudim <max.hermsdorff@gmail.com>
2021-05-08 23:41:35 -0300

    Req 02 - MovieList

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, subtitle, storyline, imagePath, id } = movie;
     return (
       <div data-testid="movie-card">
         Movie Card
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,14 +9,29 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies = async () => {
+    this.setState({ loading: true }, async () => {
+      const requestMovies = await movieAPI.getMovies();
+      this.setState({
+        movies: requestMovies,
+        loading: false,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    if (loading) return <Loading />;
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}

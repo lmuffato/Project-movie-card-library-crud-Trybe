a78d85610972ecbab390692db068165e69cf9bc4
NilsonRCS <nilson.ribeiro.souza3@gmail.com>
2021-05-09 19:22:44 -0300

    Commit de agradecimento, no arquivo index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Loading } from './Loading';
 export { default as MovieForm } from './MovieForm';
 export { default as MovieCard } from './MovieCard';
+
+// agradeço aos colegas de Trybo: Gabriel Ferreira, Luciano Amâncio, Murilo Gonçalvez, Lucas Pedroso pelo auxilio no projeto,VQV

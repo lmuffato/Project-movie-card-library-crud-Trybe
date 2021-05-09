9af0817d037ba8f08d2ee47e86bd5b40e559c711
Stanrley Barbosa <3011stan@gmail.com>
2021-05-09 19:49:29 -0300

    Requisito 2

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, id, storyline, imagePath } = movie;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <p>{ title }</p>
+        <img src={ imagePath } alt={ `Capa do filme ${title}` } width="300px" />
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    id: PropTypes.number,
+    storyline: PropTypes.string,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
@@ -9,17 +11,48 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+
+    this.getDataApi = this.getDataApi.bind(this);
+    this.modifiedRender = this.modifiedRender.bind(this);
+  }
+
+  componentDidMount() {
+    this.getDataApi();
+  }
+
+  async getDataApi() {
+    const { getMovies } = movieAPI;
+    this.setState({
+      loading: true,
+    });
+    const res = await getMovies();
+    this.setState({
+      movies: res,
+      loading: false,
+    });
+  }
+
+  modifiedRender = () => {
+    const { movies, loading } = this.state;
+
+    if (loading) return <Loading />;
+    // console.log(movies);
+    return (
+      <div>
+        { movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />) }
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      </div>
+    );
   }
 
   render() {
-    const { movies } = this.state;
-
     // Render Loading here if the request is still happening
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+      <div data-testeid="movie-list">
+        { this.modifiedRender()}
       </div>
     );
   }

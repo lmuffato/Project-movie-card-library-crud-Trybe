df36cd96d564114fdd020b4097fdbf6374cd33fc
AdelinoJnr <adelinojunior96@gmail.com>
2021-05-09 11:44:45 -0300

    Correção do requisito 1 'Descomentando codigo'

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
         <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
         <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
         <Route exact path="/" component={ MovieList } />
         <Route path="*" component={ NotFound } />
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,10 +5,11 @@ import PropTypes from 'prop-types';
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, id, storyline } = movie;
+    const { title, id, storyline, imagePath } = movie;
     return (
       <section data-testid="movie-card">
         <p>{ title }</p>
+        <img width="300" src={ imagePath } alt={ `Imagem do filme ${title}` } />
         <p>{ storyline }</p>
         <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </section>
@@ -21,6 +22,7 @@ MovieCard.propTypes = {
     title: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
     storyline: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
   }).isRequired,
 };
 

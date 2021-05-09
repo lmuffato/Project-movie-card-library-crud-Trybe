9e9dbff21cd8a160e1f5f356c27f03bf3467720c
Maria Lisboa <marialisboa1515@gmail.com>
2021-05-08 21:53:57 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NewMovie from './pages/NewMovie';
 import NotFound from './pages/NotFound';
 import EditMovie from './pages/EditMovie';
 
+// adicionei as rotas
 function App() {
   return (
     <BrowserRouter>

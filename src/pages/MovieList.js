085c2da0e6dadf3c1cbca356d26319ab9769e223
Rafael Ribeiro <rafittu@outlook.com>
2021-05-09 09:09:44 -0300

    corrige requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -31,7 +32,7 @@ class MovieList extends Component {
     const { movies, loading } = this.state;
 
     if (loading) {
-      return (<span>Carregando...</span>);
+      return <Loading />;
     }
 
     return (

577dbaadf3313a065632bb58992013e08f72bdb2
AdelinoJnr <adelinojunior96@gmail.com>
2021-05-09 16:35:44 -0300

    eslint, passa meu querido

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import loading from './esquerda-direita.gif';
 
 class Loading extends Component {
   render() {
     return (
       <div className="content-loading">
-        <div className="loading-filho">
-          <img width="200" className="gif-loading" src={ loading } alt="Gif loading" />
-          <p className="loading-text">Carregando...</p>
-        </div>
+        Carregando...
       </div>
     );
   }

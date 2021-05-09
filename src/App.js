cd30036aa6f3b2646ad3a2e184e564a70aabe039
johnpligeiro <johnpligeiro@gmail.com>
2021-05-09 02:32:25 -0300

    1.1, 1.2 e 1.5 ok

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        {/* <Route path="/movies/:id/edit" component={ EditMovie } /> */}
+        <Route path="/movies/:id" component={ MovieDetails } />
+        {/* <Route path="/movies/new" component={ NewMovie } /> */}
+        <Route exact path="/" component={ MovieList } />
+        <Route path="" component={ NotFound } />
+      </Switch>
     </Router>
   );
 }

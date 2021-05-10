a1e1932fdb18742d9b842f2b0b501c8dd5b76555
mallmann02 <leomallmann020302@gmail.com>
2021-05-10 16:35:45 -0300

    feat: Able Now to Create a New Movie ✨

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router';
 
-import MovieForm from '../components/MovieForm';
+import { MovieForm, Loading } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      status: 'running',
+      shouldRedirect: false
+    }
+    
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(newMovie) {
+    this.setState(
+      { status: 'loading' },
+      async () => {
+        await movieAPI.createMovie(newMovie);
+        this.setState({ 
+          status: 'running',
+          shouldRedirect: true
+        });
+      })
   }
 
   render() {
+    const { status, shouldRedirect } = this.state;
+    
+    if (shouldRedirect) return <Redirect to="/"/>
+
+    if ( status === 'loading' ) {
+      return (
+        <Loading />
+      );
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />

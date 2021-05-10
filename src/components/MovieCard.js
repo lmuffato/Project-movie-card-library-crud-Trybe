1d5b0b2ccf3e94ee41238dbf017ac273dbcbb164
luanlsr <luanfswd@gmail.com>
2021-05-10 18:24:37 -0300

    requisito 4 finalizado

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
-import './MovieCard.css';
 
 class MovieCard extends React.Component {
   constructor(props) {

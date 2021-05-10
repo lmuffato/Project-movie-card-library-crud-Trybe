92263100a0f19372dd3d8d4800190e171a5e851c
Lucas Godoi <lucazgodoy1996@gmail.com>
2021-05-10 15:38:00 -0300

    feat(requirement 01): :rocket: Put routes in BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route path="/" component={ MovieList } />
+      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      <Route path="*" component={ NotFound } />
+    </BrowserRouter>
   );
 }
 

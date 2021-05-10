c290d5fc99b61bcb768711a1804ae0052faf5e54
Beatriz Estebanez <bia.estebanez@gmail.com>
2021-05-10 12:21:38 -0300

    Requirement 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id, imagePath } } = this.props;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ `../../public/${imagePath}` } alt="movie" width="100px" />
+        <h1>{ title }</h1>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }

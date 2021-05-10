c4487791f55b32df6ca24ccc5771623a0670a8c3
Lucas André <lucas.cancado89@gmail.com>
2021-05-10 15:20:42 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Route exact path="/" component={ MovieList } />
         <Route path="/movies/new" component={ NewMovie } />
         <Route exact path="/movies/:id/edit" component={ EditMovie } />
-        <Route path="/movies/:id" render={ () => <MovieDetails /> } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
         <Route path="/:any" component={ NotFound } />
       </Switch>
     </Router>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,65 @@
 import React, { Component } from 'react';
-
-import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { getMovie } from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      didNotMount: true,
+      movie: {},
+    };
+  }
+
+  componentDidMount() {
+    this.returnMyMovie();
+  }
+
+  returnMyMovie = async () => {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    const myMovie = await getMovie(id);
+    this.setState({ movie: myMovie });
+    this.setState({ didNotMount: false });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie, didNotMount } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
 
     return (
       <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+        {
+          didNotMount === true
+            ? <Loading />
+            : <div>
+              <img alt="Movie Cover" src={ `../${imagePath}` } />
+              <h1>{ `Title: ${title}` }</h1>
+              <p>{ `Subtitle: ${subtitle}` }</p>
+              <p>{ `Storyline: ${storyline}` }</p>
+              <p>{ `Genre: ${genre}` }</p>
+              <p>{ `Rating: ${rating}` }</p>
+              <hr />
+              <button type="button"><Link to={ `/movies/${id}/edit` }>EDITAR</Link></button>
+              <button type="button"><Link to="/">VOLTAR</Link></button>
+            </div>
+        }
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.objectOf(PropTypes.object).isRequired,
+};
 
 export default MovieDetails;

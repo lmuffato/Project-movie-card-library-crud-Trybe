f8ac1d04b04e044287d0acdef01aa1e42b584fc1
Ana Ventura <anaventura1811@gmail.com>
2021-05-10 12:15:23 -0300

    Requisito 2

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-// import * as movieAPI from '../services/movieAPI';
-// import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
@@ -10,20 +9,39 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.handleMovies();
+  }
 
-    // Render Loading here if the request is still happening
+  handleMovies = async () => {
+    this.setState({ loading: true }, () => {
+      movieAPI.getMovies()
+        .then((data) => {
+          this.setState({ movies: data, loading: false });
+        });
+    });
+  }
 
+  showMovies(movies) {
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }
+
+  render() {
+    const { movies, loading } = this.state;
+
+    return loading ? <Loading /> : this.showMovies(movies);
+  }
 }
 
 export default MovieList;
+
+// Para entender onde colocar o loading, consultei o PR do colega Rafael Medeiros:
+// https://github.com/tryber/sd-010-a-project-movie-card-library-crud/pull/67/files

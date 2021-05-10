8b3c9eaabbd418ee7bbf32bec2dc612d0e62029e
mallmann02 <leomallmann020302@gmail.com>
2021-05-10 09:57:39 -0300

    feat: Render BrowserRouter in App Component ✨

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import NewMovie from './pages/NewMovie';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route path="/movies/:id/edit" component={EditMovie}/>
+      <Route path="/movies/:id" component={MovieDetails}/>
+      <Route path="/movies/new" component={NewMovie}/>
+      <Route path="/" exact component={MovieList}/>
+      <Route path="*" component={NotFound}/>
+    </BrowserRouter>
   );
 }
 

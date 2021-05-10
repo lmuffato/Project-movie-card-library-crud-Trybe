a35f56f91991682ff88350e311d36dcb9f6fa725
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-05-10 19:08:03 -0300

    erro lint

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
-    const { title, subtitle , genre, imagePath, rating, storyline, id} = movie;
+    const { title, subtitle, imagePath, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
@@ -16,5 +17,17 @@ class MovieCard extends React.Component {
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    subtitle: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    imagePath: PropTypes.string.isRequired,
+    rating: PropTypes.number.isRequired,
+    storyline: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 

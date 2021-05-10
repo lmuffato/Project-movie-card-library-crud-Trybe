87726368ea64b3c18bd8a678224d157a80d42d14
andy silva <andssilva.23@gmail.com>
2021-05-10 16:21:35 -0400

    feat (requeriment 2): :rocket: Cria fetchMovies

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,17 +9,38 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  async fetchApi() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const result = await movieAPI.getMovies();
+        this.setState({
+          movies: result,
+          loading: false,
+        });
+      },
+    );
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
+    const elementLoading = <span>Carregando...</span>;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { loading
+          ? elementLoading
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

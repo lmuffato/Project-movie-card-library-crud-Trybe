dc4c5cac7e4c0f09c2ea6bdc5f272eb36797bd48
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-10 18:52:09 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <div>willianAlv</div>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" componente={ MovieList } />
+        <Route exact path="/movies/:id" componente={ MovieDetails } />
+        <Route exact path="/movies/new" componente={ NewMovie } />
+        <Route exact path="/movies/:id/edit" componente={ EditMovie } />
+        <Route exact path="*" componente={ NotFound } />
+      </Switch>
+    </Router>
   );
 }
 

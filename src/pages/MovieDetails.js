2d3e9b0afec298b4a51efcb550f205dd0090ac1e
mallmann02 <leomallmann020302@gmail.com>
2021-05-10 11:29:06 -0300

    feat: Show Specific Movie Based On It's ID ✨

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,14 +4,48 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      loading: false
+    }
+  }
+
+  fetchApi = () => {
+    const { id } = this.props.match.params;
+    this.setState(
+      { loading: true },
+      async () => {
+        const movie = await movieAPI.getMovie(id);
+        this.setState({
+          movie,
+          loading: false,
+        })
+      });
+  }
+
+  componentDidMount = () => {
+    this.fetchApi();
+  }
+  
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    
+    const { loading } = this.state;
+    const { movie: { title, storyline, imagePath, genre, rating, subtitle } } = this.state;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    if (loading) {
+      return (
+        <Loading />
+      );
+    }
 
     return (
       <div data-testid="movie-details">
+        <h1>{ title }</h1>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>

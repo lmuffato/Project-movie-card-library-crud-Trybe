40e30bad3f3bb8851837a81b9c579eb23341ce39
Walvesb65 <willian.alvesb65@gmail.com>
2021-05-10 19:10:49 -0300

    Requisito 1 corrigindo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
       <Switch>
-        <Route exact path="/" componente={ MovieList } />
-        <Route exact path="/movies/:id" componente={ MovieDetails } />
-        <Route exact path="/movies/new" componente={ NewMovie } />
-        <Route exact path="/movies/:id/edit" componente={ EditMovie } />
-        <Route exact path="*" componente={ NotFound } />
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="*" component={ NotFound } />
       </Switch>
     </Router>
   );

97ae1cbcd0e481c9f3603ebbadd437198791a760
mallmann02 <leomallmann020302@gmail.com>
2021-05-10 17:15:44 -0300

    lint: Writing Patterns

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/movies/:id/edit" component={EditMovie}/>
-        <Route path="/movies/new" component={NewMovie}/>
-        <Route path="/movies/:id" component={MovieDetails}/>
-        <Route path="/" exact component={MovieList}/>
-        <Route path="*" component={NotFound}/>
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/" exact component={ MovieList } />
+        <Route path="*" component={ NotFound } />
       </Switch>
     </BrowserRouter>
   );

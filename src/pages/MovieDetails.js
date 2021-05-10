061b125d9a8b956815ddc8a8d5b258efcbf79f2c
Paulo Vitor Borges <pvtrybe209@gmail.com>
2021-05-10 18:57:31 -0300

    erro lint

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -53,4 +53,12 @@ class MovieDetails extends Component {
   }
 }
 
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  }).isRequired,
+};
+
 export default MovieDetails;

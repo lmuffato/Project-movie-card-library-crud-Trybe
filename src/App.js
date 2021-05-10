8d845a52133678f8b77943915644f2005ea1fa20
LucasMLara <lucasml@live.com>
2021-05-10 19:24:24 -0300

    feat: :sparkles: BrowserRouter Implement on App.js
    Adding Routes on App.js as required

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
+
+// Requisito feito com auxílio de Rafael Medeiros - turma 10 - A
 
 function App() {
   return (
     <Router>
-      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route exact path="*" component={ NotFound } />
+      </Switch>
     </Router>
   );
 }

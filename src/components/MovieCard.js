6e362c49ce90bba3f08d0709e82082fcdbeae2d4
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-05-10 19:38:26 -0300

    Adiciona requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{ movie.title }</h1>
+        <p>{ movie.storyline }</p>
+        <Link to={`/movies/${movie.id}`} >VER DETALHES</Link>
       </div>
     );
   }

4e183c59357d8985b3e23501c2afa945e0b93323
mallmann02 <leomallmann020302@gmail.com>
2021-05-10 10:36:16 -0300

    feat: Apply Conditional Render and fetch API ✨

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,17 +10,29 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: false,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  fetchMovies = async () => {
+    this.setState(
+      { loading: true }, 
+      async () => {
+        const movies = await movieAPI.getMovies();
+        this.setState({movies, loading: false});
+      });
+  }
 
-    // Render Loading here if the request is still happening
+  componentDidMount = () => {
+    this.fetchMovies();
+  }
+
+  render() {
+    const { movies, loading } = this.state;
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading /> : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

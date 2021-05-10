23ad0b7443241039996c7149d9cc5c7bea8cbcb3
mallmann02 <leomallmann020302@gmail.com>
2021-05-10 15:51:42 -0300

    feat: The Movie Can Be Edited ✨

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,59 @@
 import React, { Component } from 'react';
+import { Redirect, Route } from 'react-router-dom';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      status: 'running',
+      shouldRedirect: false
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    this.setState(
+      { status: 'loading' },
+      async () => {
+        await movieAPI.updateMovie(updatedMovie);
+        this.setState({
+          status: 'running',
+          shouldRedirect: true
+        })
+      });
+  }
+
+  fetchAPI = () => {
+    const { id } = this.props.match.params;
+    this.setState(
+      { status: 'loading' },
+      async () => {
+        const movie = await movieAPI.getMovie(id);
+        this.setState({
+          status: 'running',
+          movie
+        })
+      })
+  }
+
+  componentDidMount = () => {
+    this.fetchAPI();
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/"/>
     }
 
     if (status === 'loading') {
-      // render Loading
+      return(
+        <Loading />
+      );
     }
 
     return (

b6970d357ec0c2bee6cef90ba82738262d013b61
Stanrley Barbosa <3011stan@gmail.com>
2021-05-10 12:48:13 -0300

    Teste MovieDetails.test.js

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      loading: false,
+    };
+    this.getMovieById = this.getMovieById.bind(this);
+    this.modifiedRender = this.modifiedRender.bind(this);
+    this.deleteMovieLocal = this.deleteMovieLocal.bind(this);
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  componentDidMount() {
+    this.getMovieById();
+  }
 
+  async getMovieById() {
+    const { getMovie } = movieAPI;
+    const { match } = this.props;
+    const { id } = match.params;
+    this.setState({
+      loading: true,
+    });
+    const res = await getMovie(id);
+    this.setState({
+      movies: res,
+      loading: false,
+    });
+  }
+
+  deleteMovieLocal = () => {
+    const { deleteMovie } = movieAPI;
+    const { match } = this.props;
+    const { id } = match.params;
+    deleteMovie(id);
+  }
+
+  modifiedRender = () => {
+    const { movies, loading } = this.state;
+    const { imagePath, subtitle, storyline, genre, rating, title } = movies;
+    const { match } = this.props;
+    const { id } = match.params;
+    if (loading) return <Loading />;
     return (
-      <div data-testid="movie-details">
+      <section>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
@@ -20,10 +57,24 @@ class MovieDetails extends Component {
         <div id="teste">
           <spam>{ `movieAPI: ${movieAPI}; Loading: ${Loading} ; Title:${title}` }</spam>
         </div>
-      </div>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+        <Link to="/" onClick={ this.deleteMovieLocal }>DELETAR</Link>
+      </section>
+    );
+  }
 
+  render() {
+    return (
+      <div data-testid="movie-details">
+        { this.modifiedRender() }
+      </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.object,
+}.isRequerid;
 
 export default MovieDetails;

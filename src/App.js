c2e52e8646586e3716675de00f254b4d7d36546c
Lucas Pedroso <lepsk8@gmail.com>
2021-05-10 16:46:47 -0300

    feat: :sparkles: Add BroserRouter and Route's

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import { MovieList, EditMovie, NewMovie, MovieDetails } from './pages';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    
+    <BrowserRouter>
+      <Route path="/" component={MovieList} />
+      <Route path="/movies/:id" component={MovieDetails} />
+      <Route path="/movies/new" component={NewMovie} />
+      <Route path="/movies/:id/edit" component={EditMovie} />
+    </BrowserRouter>
+
+// Será validado se qualquer rota não declarada renderiza a página NotFound
   );
 }
 

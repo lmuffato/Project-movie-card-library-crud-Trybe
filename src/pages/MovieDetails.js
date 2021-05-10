c74cfc93728eb2fade5712064f350aeaa2a71e7e
MaxRudim <max.hermsdorff@gmail.com>
2021-05-10 16:00:54 -0300

    Req: 04 - feito

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,68 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
+import Loading from '../components/Loading';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie = async () => {
+    const { match } = this.props;
+    const { params } = match;
+    const { id } = params;
+    this.setState({ loading: true }, async () => {
+      const requestMovie = await movieAPI.getMovie(id);
+      this.setState({
+        movie: requestMovie,
+        loading: false,
+      });
+    });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { movie, loading } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <button type="button">
+          <Link to={ `/movies/${id}/edit` }>
+            EDITAR
+          </Link>
+        </button>
+        <button type="button">
+          <Link to="/">
+            VOLTAR
+          </Link>
+        </button>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.object,
+}.isRequired;
 
 export default MovieDetails;

ce8ffc9baebc7d32046640bc01f9f5b8b7c95b07
andy silva <andssilva.23@gmail.com>
2021-05-10 18:01:26 -0400

    feat (requeriment 4): :rocket: Cria uma requisição para buscar o filme que deverá ser renderizado

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class Loading extends Component {
   render() {
     return (
-      <div>Loading message</div>
+      <>
+        <img src="http://www.sudburycatholicschools.ca/wp-content/plugins/3d-flip-book/assets/images/dark-loader.gif" alt="carregando" />
+        <div>Carregando...</div>
+      </>
     );
   }
 }
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,82 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.state = {
+      movie: {},
+      loading: true,
+      id: '',
+    };
+  }
 
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const { match } = this.props;
+        const { params } = match;
+        const { id } = params;
+        const movie = await movieAPI.getMovie(id);
+        this.setState({
+          movie,
+          loading: false,
+          id,
+        });
+      },
+    );
+  }
+
+  renderDivOrLoading() {
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
-      <div data-testid="movie-details">
+      <>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h1>{ `Title: ${title}` }</h1>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+      </>
+    );
+  }
+
+  render() {
+    // Change the condition to check the state
+    // if (true) return <Loading />;
+    const { loading, id } = this.state;
+
+    return (
+      <div data-testid="movie-details">
+        { loading ? <Loading /> : this.renderDivOrLoading() }
+        <button type="button">
+          <Link to="/">VOLTAR</Link>
+        </button>
+        <button type="button">
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        </button>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default MovieDetails;

aa0abc0961410b293e9a5dbc6482e092dba6b01e
andy silva <andssilva.23@gmail.com>
2021-05-10 15:57:36 -0400

    feat (requeriment 1): :rocket: Renderiza BrowserRouter no componente App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+        <Route path="*" component={ NotFound } />
+        <div>Movie Card Library CRUD</div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;

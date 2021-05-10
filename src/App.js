cbcd2efbc6149c2f48259c23ef0b555a71f05889
Edmilson Bernardo <ebedmilson48@gmail.com>
2021-05-10 18:37:56 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <Route path="/" component={ MovieList } exact />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route component={ NotFound } />
+    </BrowserRouter>
   );
 }
 

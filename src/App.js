7c891688fe003a66ca0d3aebc8ae193714a69e2f
Maria Luiza Castelar <mluiza.vcr@gmail.com>
2021-05-10 16:53:52 -0300

    Commit inicial - Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends Component.React {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>Movie Card Library CRUD</div>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route
+          path="/movies/:id/edit"
+          render={ (props) => <EditMovie { ...props } /> }
+        />
+        <Route path="*" component={ NotFound } />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;

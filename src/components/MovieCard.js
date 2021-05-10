54c1b83aa0dcdb068b1ee020f43ff053421f61a1
andy silva <andssilva.23@gmail.com>
2021-05-10 16:55:02 -0400

    feat (requeriment 3): :rocket: Cria um link para página de detalhes de um filme

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, id } = movie;
     return (
       <div data-testid="movie-card">
         Movie Card
+        <h1>{ title }</h1>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    storyline: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+  }).isRequired,
+};
 
 export default MovieCard;

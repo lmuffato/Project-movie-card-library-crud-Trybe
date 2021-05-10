2a5249a92931ba09538f055985e6ec3aee67822b
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-05-10 19:07:31 -0300

    Requisito 6

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -36,6 +36,7 @@ class MovieList extends Component {
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
       </div>
     );
   }

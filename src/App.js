77c541d8fe3ae85cbcfa84a85955e1c6f7a82377
Elisa <elisafranca.carvalho@gmail.com>
2021-05-10 10:55:27 -0300

    4th requirement

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Route exact path="/" component={ MovieList } />
-      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
       <Route path="/movies/new" component={ NewMovie } />
       <Route path="/movies/:id/edit" component={ EditMovie } />
       {/* Referência para a rota da página não encontrada: https://qastack.com.br/programming/32128978/react-router-no-not-found-route */}
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
@@ -7,7 +8,7 @@ class MovieDetails extends Component {
   constructor() {
     super();
     this.state = {
-      movie: undefined,
+      movie: {},
       loading: true,
     };
   }
@@ -17,8 +18,10 @@ class MovieDetails extends Component {
   }
 
   async fetchMovieApi() {
+    // Para a requisição pelo id, contei com o auxílio do pull request do Luciano Amâncio;
+    const { match: { params: { id } } } = this.props;
     this.setState({ loading: true }, async () => {
-      const getMovieReturn = await movieAPI.getMovie();
+      const getMovieReturn = await movieAPI.getMovie(id);
       this.setState({
         movie: getMovieReturn,
         loading: false,
@@ -30,18 +33,29 @@ class MovieDetails extends Component {
     // Change the condition to check the state
     // if (true) return <Loading />;
     const { movie, loading } = this.state;
-    const { storyline, imagePath, genre, rating, subtitle } = movie;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return !loading ? (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
       </div>
     ) : (<Loading />);
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;

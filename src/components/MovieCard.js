e870227beb86f88bf4d953392dcc56e426e13186
igson <igson660@gmail.com>
2021-05-11 14:33:00 -0500

    quesito 03

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { title, storyline, id } = movie;// const { movie: { title, storyline, id } } = this.props; de ser feita essa descontrução, mais ainda não aprendi como funciona
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h3>{ title }</h3>
+        <p>{ storyline }</p>
+        <Link to={ `/movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: propTypes.shape(propTypes.object),
+  title: propTypes.string,
+  storyline: propTypes.string,
+}.isRequired;
 
 export default MovieCard;

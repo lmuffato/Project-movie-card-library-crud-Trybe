cb59f1de661cfbc4903a9b7ac0d1e1bc4c40fd9a
hheloisa <hhackenhaar@gmail.com>
2021-05-11 20:31:56 -0300

    resolve alguns erros do lint e inicia requisito 5

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    const { id, title, storyline, rating, imagePath } = movie;
+    const idLink = `/movies/${id}`;
     return (
-      <div data-testid="movie-card">
-        Movie Card
+      <div className="movie-card" data-testid="movie-card">
+        <img
+          alt="Movie Cover"
+          className="movie-card-image"
+          src={ imagePath }
+        />
+        <div className="movie-card-body">
+          <h4 data-testid="movie-card-title" className="movie-card-title">{title}</h4>
+          <p className="movie-card-storyline">{storyline}</p>
+        </div>
+        <div className="movie-card-rating" data-testid="rating">
+          <span className="rating">{rating}</span>
+          <Link to={ idLink }>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    imagePath: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;
--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,20 +1,36 @@
 import React, { Component } from 'react';
-
+import Loading from '../components/Loading';
 import { MovieForm } from '../components';
 // import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+    this.showMovieForEdit = this.showMovieForEdit.bind(this);
     // this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.showMovieForEdit();
+  }
+
+  showMovieForEdit = async () => {
+    const { match: { params: { id } } } = this.props;
+    const specificMovie = await movieAPI.getMovie(id);
+    this.setState({
+      movie: specificMovie,
+      loading: false,
+    });
+  }
   /*   handleSubmit(updatedMovie) {
   } */
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { status, shouldRedirect, movie, loading } = this.state;
     if (shouldRedirect) {
       // Redirect
     }
@@ -23,12 +39,20 @@ class EditMovie extends Component {
       // render Loading
     }
 
-    return (
+    return loading ? <Loading /> : (
       <div data-testid="edit-movie">
         <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
       </div>
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.number, PropTypes.string]),
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;

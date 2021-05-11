aa2a2a6e5a10cfa70adde94e326ed83c3df2ed14
Lucas Pedroso <lepsk8@gmail.com>
2021-05-10 22:10:33 -0300

    feat: :sparkles: Add MovieCard component
    implemented render()

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { id, title, subtitle, storyline, imagePath, rating, genre } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <span className="movieId" style={{display: 'none'}}>{id}</span>
+        <img src={imagePath} alt={title} />
+        <h1>{title}</h1>
+        <h2>{subtitle}</h2>
+        <p>{storyline}</p>
+        <p>{genre}</p>
+        <span>{rating}</span>
+        <Link to={`/movies/${id}`}>VER DETALHES</Link>
       </div>
     );
   }

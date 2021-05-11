891fc67d5aa7ec04dfc627d4d1444993b5c1bb21
hheloisa <hhackenhaar@gmail.com>
2021-05-11 20:37:15 -0300

    comenta codigos tentando resolver o lint, apontando erros onde eu nem cheguei

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 
 import MovieForm from '../components/MovieForm';
-import * as movieAPI from '../services/movieAPI';
+// import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
+  // {//newMovie é parametro de handlesubmit}
 
-  handleSubmit(newMovie) {
+  handleSubmit() {
+    console.log('test');
   }
 
   render() {

298d0dd2a2ecbe37f4d1d186a229e583ed16aae7
Mateus Martins - Turma 10 - Tribo A <mateusmartinsmck@gmail.com>
2021-05-11 17:17:24 -0300

    Requisito 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img src={ movie.imagePath } alt={ `Imagem de ${movie.title}` } />
+        <div>
+          <h2>{movie.title}</h2>
+          <h4>{movie.subtitle}</h4>
+          <p>{movie.storyline}</p>
+        </div>
+        <div>
+          <Link to={ `/movies/${movie.id}` }>VER DETALHES</Link>
+        </div>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    imagePath: PropTypes.string,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    id: PropTypes.number,
+  }).isRequired,
+};
 
 export default MovieCard;

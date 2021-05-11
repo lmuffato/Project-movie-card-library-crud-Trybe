c8505f753f65e08ce7347c2533c411ddda8d97f5
Lucas Pedroso <lepsk8@gmail.com>
2021-05-10 22:23:55 -0300

    feat: :truck: Add 4 routes
    /, /movies/new, /movies:id:edit, /movies/:id

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MovieList, EditMovie, NewMovie, MovieDetails } from './pages';
 
 function App() {
   return (
     
     <BrowserRouter>
-      <Route path="/" component={MovieList} />
-      <Route path="/movies/:id" component={MovieDetails} />
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="/movies/:id/edit" component={EditMovie} />
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route exact path="/movies/new" component={NewMovie} />
+        <Route exact path="/movies/:id/edit" component={EditMovie} />
+        <Route exact path="/movies/:id" component={MovieDetails} />
+      </Switch>
     </BrowserRouter>
 
 // Será validado se qualquer rota não declarada renderiza a página NotFound

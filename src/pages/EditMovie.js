35db20b9952ae9df5231325ac0c3bc68cd69a713
igson <igson660@gmail.com>
2021-05-11 17:51:19 -0500

    quesito 05

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
-
+import { number } from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      shouldRedirect: false,
+      status: 'loading',
+      movie: {},
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    this.setState({ shouldRedirect: false }, async () => {
+      await movieAPI.updateMovie(updatedMovie);
+      this.setState({ shouldRedirect: true });
+    });
+  }
+
+  // função feita com a ajuda do Luan Ramalho
+  fetchMovie = async () => {
+    const { match: { params: { id } } } = this.props;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      status: 'updated',
+      movie,
+    });
+  }
+
+  componentDidMount = async () => {
+    this.fetchMovie();
   }
 
   render() {
     const { status, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
     if (status === 'loading') {
-      // render Loading
+      return <Loading />;
     }
 
     return (
@@ -30,5 +54,9 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  id: number,
+}.isRequired;
 
 export default EditMovie;

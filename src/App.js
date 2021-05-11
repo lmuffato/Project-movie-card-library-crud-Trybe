22239ca65abfa072e3eac71a90d1d72cd2b55a88
hheloisa <hhackenhaar@gmail.com>
2021-05-11 16:25:00 -0300

    Requisitos 1 e 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFund from './pages/NotFound';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="*" component={ NotFund } />
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
 
 class MovieList extends Component {
   constructor() {
@@ -12,15 +12,28 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    this.findShowMovies();
+  }
+
+  findShowMovies = async () => {
+    const { getMovies } = movieAPI;
+    this.setState({
+      movies: await getMovies(),
+    });
+  }
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
-
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      <section>
+        <Loading />
+        <div data-testid="movie-list">
+          {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        </div>
+      </section>
     );
   }
 }

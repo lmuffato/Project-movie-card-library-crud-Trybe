189908e5186dc292759ff0384ac48cbf5b73c86b
Gustavo Sousa <gustavohl98@gmail.com>
2021-05-11 12:52:15 -0300

    Requisito 03 - insere titulo, sinopse e Link dentro de cada MovieCard

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie } = this.props;
+    console.log(movie);
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h2>{movie.title}</h2>
+        <p>{movie.storyline}</p>
+        <Link to={ `/movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    subtitle: PropTypes.string,
+    storyline: PropTypes.string,
+    rating: PropTypes.number,
+    imagePath: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;

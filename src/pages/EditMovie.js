f6419422409919bed442bc36a853b314b87a6755
Lucas Pedroso <lepsk8@gmail.com>
2021-05-10 22:12:27 -0300

    feat: :sparkles: Implement handleSubmit
    componentDiMount and add Redirect

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from '../components/Loading';
 
 import { MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
@@ -6,21 +8,42 @@ import * as movieAPI from '../services/movieAPI';
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loading: true,
+      shouldRedirect: false,
+      movie: [],
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie)
+      .then((movieUpdate) => {
+        console.log(movieUpdate);
+        this.setState({
+          shouldRedirect: true,
+        });
+      });
+  }
+
+  componentDidMount() {
+    const { match: { params: { id } } } = this.props;
+    movieAPI.getMovie(id).then((movie) => {
+      this.setState({
+        loading: false,
+        movie,
+      });
+    });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { loading, shouldRedirect, movie } = this.state;
     if (shouldRedirect) {
-      // Redirect
+      return <Redirect to="/" />;
     }
 
-    if (status === 'loading') {
-      // render Loading
+    if (loading) {
+      return <Loading />
     }
 
     return (

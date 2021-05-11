c6535cfd6719c84e554e7f5ed0488fc1dc1bd88b
guidugaich <guidugaich.dev@gmail.com>
2021-05-11 18:35:52 -0300

    requisito 2 - renderizando movie cards condicionalmente

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -14,16 +14,29 @@ class MovieList extends Component {
     };
   }
 
-  render() {
+  componentDidMount = async () => {
+    this.setState({loading:  true})
+    const movieListFromAPI = await movieAPI.getMovies();
+    this.setState({movies: movieListFromAPI, loading: false}) 
+  }
+
+  renderMovieList = () => {
     const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
+  }
+
+  render() {
+    const { loading } = this.state;
+    // Render Loading here if the request is still happening
+    return (
+      <div>
+        {loading ? <Loading /> : this.renderMovieList()}
+      </div>
+    )
   }
 }
 

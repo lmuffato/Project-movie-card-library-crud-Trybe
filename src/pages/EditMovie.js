3d195575a9395e9aa9a358c3a7c113ebcd65c1c7
Maria Baeta <m.martinsdg@gmail.com>
2021-05-11 10:02:17 -0300

    Requisito 5

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,31 +1,79 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import { MovieForm } from '../components';
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      shouldRedirect: false,
+      movie: [],
+      status: true,
+    };
+
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.getMovie = this.getMovie.bind(this);
   }
 
-  handleSubmit(updatedMovie) {
+  componentDidMount() {
+    this.getMovie();
+  }
+
+  async handleSubmit(updatedMovie) {
+    const { updateMovie } = movieAPI;
+    this.setState(async () => {
+      const updateResult = await updateMovie(updatedMovie);
+      this.setState({
+        movie: updateResult,
+        shouldRedirect: true,
+      });
+    });
+  }
+
+  async getMovie() {
+    const { match: { params: { id } } } = this.props;
+    this.setState({ status: true }, async () => {
+      const Movie = await movieAPI.getMovie(id);
+      this.setState({
+        movie: Movie,
+        status: false,
+      });
+    });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
-    if (status === 'loading') {
-      // render Loading
-    }
+    const { status, movie, shouldRedirect } = this.state;
     return (
-      <div data-testid="edit-movie">
-        <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+      <div>
+        {shouldRedirect && <Redirect to="/" />}
+        {status ? <Loading /> : (
+          <div data-testid="edit-movie">
+            <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+          </div>
+        )}
       </div>
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.objectOf(PropTypes).isRequired,
+};
+
 export default EditMovie;
+
+// if (shouldRedirect) {
+//   // Redirect
+// }
+// if (status === true) {
+//   // render Loading
+//   this.loading();
+// }
+// return (
+//   <div data-testid="edit-movie">
+//     <MovieForm movie={ movie } onSubmit={ this.handleSubmit } />
+//   </div>
+// )
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,61 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Loading from '../components/Loading';
+import * as movieAPI from '../services/movieAPI';
 // import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: [],
+      load: true,
+    };
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    this.getMovie = this.getMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.getMovie();
+  }
+
+  async getMovie() {
+    const { match: { params: { id } } } = this.props;
+    this.setState({ load: true }, async () => {
+      const Movie = await movieAPI.getMovie(id);
+      this.setState({
+        movie: Movie,
+        load: false,
+      });
+    });
+  }
+
+  render() {
+    const { movie, load } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div>
+        { load ? <Loading /> : (
+          <div data-testid="movie-details">
+            <img alt="Movie Cover" src={ `../${imagePath}` } />
+            <p>{`Title: ${title}`}</p>
+            <p>{`Subtitle: ${subtitle}`}</p>
+            <p>{`Storyline: ${storyline}`}</p>
+            <p>{`Genre: ${genre}`}</p>
+            <p>{`Rating: ${rating}`}</p>
+            <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+            <Link to="/">VOLTAR</Link>
+          </div>
+        )}
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.objectOf(PropTypes.string).isRequired,
+};
 
 export default MovieDetails;

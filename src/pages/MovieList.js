d1cc743a7a9c310a8ab0be340ffec5d3c49869a3
Lucas Pedroso <lepsk8@gmail.com>
2021-05-10 22:06:46 -0300

    feat: :sparkles: Implement MovieList.js
    create componentDidMout e Loading

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -8,18 +10,29 @@ class MovieList extends Component {
     super();
 
     this.state = {
+      loading: true,
       movies: [],
     };
   }
 
+  componentDidMount() {
+    movieAPI.getMovies().then((moviesList) => {
+      this.setState({
+        loading: false,
+        movies: moviesList,
+      });
+    });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+        {loading ? <Loading /> : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

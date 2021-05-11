30f39debbb9cb514fa4229a4d903b63c0c83239f
guidugaich <guidugaich.dev@gmail.com>
2021-05-11 20:25:22 -0300

    requisito 4 parcialmente resolvido

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      loading: false,
+    };
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  fetchMovie = () => {
+    const { match } = this.props;
+    const { id } = match.params;
+    this.setState(
+      { loading: true },
+      async () => {
+        const movieFromAPI = await movieAPI.getMovie(id);
+        this.setState({
+          movie: movieFromAPI,
+          loading: false,
+        });
+      },
+    );
+  }
 
+  renderMovieDetails = () => {
+    const { movie } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
@@ -20,6 +43,28 @@ class MovieDetails extends Component {
       </div>
     );
   }
+
+  componentDidMount = () => {
+    this.fetchMovie();
+  }
+
+  render() {
+    const { loading } = this.state;
+    return (
+      <div>
+        {loading ? <Loading /> : this.renderMovieDetails()}
+      </div>
+    );
+  }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    isExact: PropTypes.bool,
+    params: PropTypes.objectOf(PropTypes.number),
+    path: PropTypes.string,
+    url: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieDetails;

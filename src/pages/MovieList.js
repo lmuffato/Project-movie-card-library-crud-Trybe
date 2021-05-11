eeece28b891b2e1c9a728a0ff740ed127a238438
Villefor <franklincrius@gmail.com>
2021-05-11 19:26:05 -0300

    2 requisito, com a ajuda do Joao!

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,18 +9,28 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
-      loading: true
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.loadMovie();
+  }
+
+  async loadMovie() {
+    const getMovie = await movieAPI.getMovies();
+    this.setState({ movies: getMovie, loading: false });
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? <Loading />
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

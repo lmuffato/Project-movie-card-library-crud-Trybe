056a0f4279ef43f54ee0cbc2a1933d4958b8c847
Rafael Medeiros <62731714+RafaelMedeirosGomes@users.noreply.github.com>
2021-05-11 18:51:47 -0300

    Small refact

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import './App.css';
 import { MovieList, MovieDetails, NewMovie, EditMovie, NotFound } from './pages';
 
 // Entire project done using https://reactrouter.com/web documentation
+// Routes order tip given by Iago
 const App = () => (
   <Router>
     <div>Movie Card Library CRUD</div>
     <Link to="/movies/new">ADICIONAR CARTÃO</Link>
     <Switch>
-      <Route exact path="/" component={ MovieList } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
       <Route exact path="/movies/new" component={ NewMovie } />
       <Route exact path="/movies/:id" component={ MovieDetails } />
-      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route exact path="/" component={ MovieList } />
       <Route component={ NotFound } />
     </Switch>
   </Router>

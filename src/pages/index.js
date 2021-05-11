273262e7c32f47b10d39fdbb4d222ca9c065753d
jonathan <jonathanfernandesjf98@gmail.com>
2021-05-11 16:38:07 -0300

    requisito 01

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Index extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Index</h1>
+      </div>
+    );
+  }
+}
+
 export { default as EditMovie } from './EditMovie';
 export { default as MovieDetails } from './MovieDetails';
 export { default as MovieList } from './MovieList';

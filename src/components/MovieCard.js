2bb432daa06e6a9fcc81ad268b742f3e65911e81
ScriptCamilo <rodrigo.camilo228@gmail.com>
2021-05-11 17:19:26 -0300

    Second requirement done

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -3,9 +3,9 @@ import React from 'react';
 class MovieCard extends React.Component {
   render() {
     return (
-      <div data-testid="movie-card">
+      <li data-testid="movie-card">
         Movie Card
-      </div>
+      </li>
     );
   }
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -3,24 +3,46 @@ import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
 
+// https://app.betrybe.com/course/front-end/ciclo-de-vida-de-componentes-e-react-router/ciclo-de-vida-de-componentes/36f2a45f-a7c0-4f6f-ae29-119286c4dce9/conteudos/6ff97820-f288-46e1-8a76-b870cb9f4402/renderizacao-condicional-e-atualizacao-de-arrays-no-estado/60fd32b9-3c45-4efa-a2e0-23c54e8bdac1?use_case=side_bar
 class MovieList extends Component {
   constructor() {
     super();
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
-  render() {
-    const { movies } = this.state;
+  componentDidMount() {
+    this.getMovies();
+  }
 
-    // Render Loading here if the request is still happening
+  async getMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const movies = await movieAPI.getMovies();
+
+        this.setState({
+          loading: false,
+          movies,
+        });
+      },
+    );
+  }
+
+  render() {
+    const { movies, loading } = this.state;
+    const loadingElement = <span>Carregando...</span>;
 
     return (
-      <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
-      </div>
+      <ul data-testid="movie-list">
+        {
+          loading ? loadingElement
+            : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)
+        }
+      </ul>
     );
   }
 }

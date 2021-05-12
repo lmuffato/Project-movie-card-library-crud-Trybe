6a0aa534d769c51463217a1b73f1e58306b97e52
LucasMLara <lucasml@live.com>
2021-05-12 00:06:42 -0300

    feat: :zap: MovieList Implement
    Movie List Implement and Loading text changed

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -8,15 +8,23 @@ class MovieList extends Component {
     super();
 
     this.state = {
-      movies: [],
+      movies: {},
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  async fetchData() {
+    const response = await movieAPI.getMovies();
+    this.setState({ movies: response, loading: false });
+  }
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
+    const { movies, loading } = this.state;
+    if (loading) return <Loading />;
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}

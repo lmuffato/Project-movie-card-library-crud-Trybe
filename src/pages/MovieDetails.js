7c2a5a7fb27a52c5b32cac988c98bd3948de01ac
guidugaich <guidugaich.dev@gmail.com>
2021-05-12 10:18:11 -0300

    ajustando lint para o MovieDetails

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -40,7 +40,7 @@ class MovieDetails extends Component {
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
-        <Link to={`/movies/${id}/edit`}>EDITAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
         <Link to="/">VOLTAR</Link>
       </div>
     );

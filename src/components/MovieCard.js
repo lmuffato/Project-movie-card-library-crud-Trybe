3e55b0224ce866bfc62d26e7ccf07ba9dfd1e20d
LucasMLara <lucasml@live.com>
2021-05-12 00:38:17 -0300

    feat: :zap: MovieCard Implement
    MovieCard react implemented

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import propTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id, imagePath } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <img className="movieCardImg" src={ `${imagePath}` } alt="MovieImage" />
+        <h4>{title}</h4>
+        <p>{storyline}</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: propTypes.shape,
+}.isRequired;
 
 export default MovieCard;

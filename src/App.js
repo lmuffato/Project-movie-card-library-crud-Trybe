dd012d8dc6d90b777ec7871bfbd4630cf7a8197b
Maria Lisboa <marialisboa1515@gmail.com>
2021-05-13 13:27:54 -0300

    requisito 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
@@ -11,20 +11,14 @@ import EditMovie from './pages/EditMovie';
 function App() {
   return (
     <BrowserRouter>
-      <Router path="/" component={ MovieList }>
-        <MovieList />
-      </Router>
-      <Router path="/movies/:id" component={ MovieDetails }>
-        <MovieDetails />
-      </Router>
-      <Router path="/movies/new" component={ NewMovie }>
-        <NewMovie />
-      </Router>
-      <Router path="/movies/:id/edit" component={ EditMovie }>
-        <EditMovie />
-      </Router>
-      <Router path="/movies/:id/edit" component={ NotFound }>
-        <NotFound />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route component={ NotFound } />
+        </Switch>
       </Router>
     </BrowserRouter>
   );

bfa1cad900c6da19af608aad1551f3dc9be93cbe
guidugaich <guidugaich.dev@gmail.com>
2021-05-13 18:28:31 -0300

    loading bonitao!

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import '../style/Loading.css'
 
 class Loading extends Component {
   render() {
     return (
-      <div>Carregando...</div>
+      <div className="loading-container">
+        <div className="spinner-1"></div>
+        <p>Carregando...</p>
+      </div>
     );
   }
 }

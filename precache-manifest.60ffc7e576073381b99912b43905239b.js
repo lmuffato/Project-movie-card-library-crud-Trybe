7a1d18c54c636183d5f68e54be3a804b287d9cb5
Bruno Mendes <mendessbruno@gmail.com>
2021-05-13 14:47:02 -0300

    Updates

---
+++ precache-manifest.60ffc7e576073381b99912b43905239b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4feb775c8c5d85ed3c23bc23df40d67",
+    "url": "/movie-card.io/index.html"
+  },
+  {
+    "revision": "d2b077b7a0b46ffb82bb",
+    "url": "/movie-card.io/static/css/main.026a6b0b.chunk.css"
+  },
+  {
+    "revision": "866f5de27f34dadb75dd",
+    "url": "/movie-card.io/static/js/2.bc63a950.chunk.js"
+  },
+  {
+    "revision": "d2b077b7a0b46ffb82bb",
+    "url": "/movie-card.io/static/js/main.5862e2b7.chunk.js"
+  },
+  {
+    "revision": "71175c58b4314b8ffd3a",
+    "url": "/movie-card.io/static/js/runtime-main.2c089147.js"
+  }
+]);

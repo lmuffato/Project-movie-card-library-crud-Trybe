b09fc0b02a281bf61d477e545c6129d14519d91a
rcalvs <rafaelcalvette@gmail.com>
2021-05-13 13:48:16 -0300

    Req 1-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-    </Router>
+      <BrowserRouter>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route path="/movies/new" component={ NewMovie } />
+        <Route path="/movies/:id/edit" component={ EditMovie } />
+        {/* https://qastack.com.br/programming/32128978/react-router-no-not-found-route */}
+        <Route path="*" component={ NotFound } />
+    </BrowserRouter>
   );
 }
 

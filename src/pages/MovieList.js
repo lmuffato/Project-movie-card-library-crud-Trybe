4f5eabe0a8118a4a753f2d9ce568961829f0d857
Nathalia Zebral <natzebral@gmail.com>
2021-05-15 11:26:42 -0300

    requisito 2 - passando no teste local

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -9,17 +10,41 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.takeMoviesFromApi();
+  }
+
+  takeMoviesFromApi = () => {
+    this.setState({
+      loading: true,
+    }, async () => {
+      const movieFromApi = await movieAPI.getMovies();
+      this.setState({
+        movies: movieFromApi,
+        loading: false,
+      });
+    });
+  }
+
+  pageToRender = () => {
+    const { movies, loading } = this.state;
+    if (loading) return <Loading />;
+    return (
+      <div>
+        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        <Link to="/movies/new">ADICIONAR CARTÃO</Link>
+      </div>
+    );
+  };
+
   render() {
-    const { movies } = this.state;
-
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { this.pageToRender() }
       </div>
     );
   }
--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import MovieForm from '../components/MovieForm';
 import * as movieAPI from '../services/movieAPI';
 
@@ -9,8 +8,8 @@ class NewMovie extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(newMovie) {
-  }
+  // handleSubmit(newMovie) {
+  // }
 
   render() {
     return (

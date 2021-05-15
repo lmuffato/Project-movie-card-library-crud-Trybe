62b3f0b99d7896a04241b7d4f28f1eb2f3c7b204
viniciusfranca019 <viniciusfrancas019@gmail.com>
2021-05-15 12:47:01 -0300

    fix App test part 2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={MovieList} />
         <Route path="/movies/new" component={NewMovie} />
-        <Route path="/movies/:id" component={MovieDetails} />
+        <Route
+          path="/movies/:id"
+          render={(props) => <MovieDetails {...props} />}
+        />
         <Route path="/movies/:id/edit" component={EditMovie} />
         <Route component={NotFound} />
       </Switch>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -3,14 +3,37 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+    };
+  }
+
+  componentDidMount() {
+    this.requestMovie();
+  }
+  
+  requestMovie = async () => {
+    const { getMovie } = movieAPI;
+    const { id } = this.props.match.params;
+    const movie = await getMovie(id);
+    this.setState({
+      movie: movie,
+    });
+  }
+    
+  
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+    const { title, storyline, imagePath, genre, rating, subtitle } = this.state;
 
     return (
       <div data-testid="movie-details">
+        <h2>{ title }</h2>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>

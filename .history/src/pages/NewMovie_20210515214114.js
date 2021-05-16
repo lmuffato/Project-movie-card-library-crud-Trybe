5373b6c9a9706c94706ef0bc61c1ea41e2b268ac
Juan Pablo <juan.tentiomaru9309@gmail.com>
2021-05-15 23:58:49 -0300

    req1 mudança de ordem dos componentes do app.js

---
+++ .history/src/pages/NewMovie_20210515214114.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+import MovieForm from '../components/MovieForm';
+import * as movieAPI from '../services/movieAPI';
+
+class NewMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(newMovie) {
+    movieAPI.getMovie(newMovie);
+  }
+
+  render() {
+    return (
+      <div data-testid="new-movie">
+        <MovieForm onSubmit={ this.handleSubmit } />
+      </div>
+    );
+  }
+}
+export default NewMovie;

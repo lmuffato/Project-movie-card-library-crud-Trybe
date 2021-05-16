b2bc840745e86d6ca48c61252005f1f94d4d7769
Vinicios <soicinivt@gmail.com>
2021-05-16 20:48:05 -0300

    Requisito 4. Implementa cards com os detalhes do filme selecionado, alem de links para edicao e de retorno a pagina principal.

--- src/App.js
+++ src/App.js
@@ -15,7 +15,11 @@ function App() {
       <Switch>
         <Route exact path="/" component={ MovieList } />
         <Route exact path="/movies/new" component={ NewMovie } />
-        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route
+          exact
+          path="/movies/:id"
+          render={ (props) => <MovieDetails { ...props } /> }
+        />
         <Route exact path="/movies/:id/edit" component={ EditMovie } />
         <Route component={ NotFound } />
       </Switch>
--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,62 @@
 import React, { Component } from 'react';
-
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor(props) {
+    super(props);
+    const { id } = props.match.params;
+    this.state = {
+      movie: '',
+      id,
+    };
+  }
+
+  componentDidMount() {
+    this.request();
+  }
+
+  request = async () => {
+    const { getMovie } = movieAPI;
+    const { id } = this.state;
+    const result = await getMovie(id);
+    this.setState({
+      movie: result,
+    });
+  };
+
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
-
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
-    return (
+    const { movie, id } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const cardIsTrue = (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
       </div>
+    );
+
+    return (
+      <section>
+        {(movie) ? cardIsTrue : <Loading />}
+      </section>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;

9036f4e63e24ef52dd4795c05e583d16607c7cdd
Southier <patrick.southier@hotmail.com>
2021-05-16 18:28:44 -0300

    Completes requirement 1.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+import './App.css';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+      <Route path="/" component={ MovieList } />
+      <Route path="/movies/:id" component={ MovieDetails } />
+      <Route path="/movies/new" component={ NewMovie } />
+      <Route path="/movies/:id/edit" component={ EditMovie } />
+      <Route path="*" component={ NotFound } />
+    </Router>
   );
 }
 

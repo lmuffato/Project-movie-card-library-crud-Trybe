9822993a5a4ef3ba008cf39e993f73744a092132
Vinicios <soicinivt@gmail.com>
2021-05-16 18:52:23 -0300

    Requisito 3. Adiciona titulo, sinopse e link para casa filme da lista.

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-class MovieCard extends React.Component {
+class MovieCard extends Component {
   render() {
+    const { movie } = this.props;
+
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <h1>{movie.title}</h1>
+        <p>{movie.storyline}</p>
+        <Link to={ `movies/${movie.id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    storyline: PropTypes.string,
+  }).isRequired,
+};
 
 export default MovieCard;

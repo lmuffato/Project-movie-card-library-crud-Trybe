4b481bf516e789dc2f3aedaf6d670d3a6ddc4611
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-05-16 11:24:21 -0300

    req 2 sem lint

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading'
-
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
     this.state = {
       movies: [],
+      loaded: true,
     };
   }
 
+  async getList() {
+    const getMovies = await movieAPI.getMovies();
+    this.setState({movies: getMovies, loaded: false}) 
+  }
+
+  componentDidMount() {
+    this.getList()
+  }
   
   render() {
-    const { movies } = this.state;
+    const { movies, loaded } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loaded ? <Loading /> : movies.map((movie) => 
+          <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

e9987f54923be2a75ab24a93a095700a3764ccc5
ThalitaC <thalitaceci@gmail.com>
2021-05-16 20:43:12 -0300

    Requisito 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
-  render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+  constructor(props) {
+    super(props);
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+    this.getMovieDetails = this.getMovieDetails.bind(this);
+  }
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
+  componentDidMount() {
+    this.getMovieDetails();
+  }
+
+  getMovieDetails = async () => {
+    const { match } = this.props;
+    const { id } = match.params;
+    const movie = await movieAPI.getMovie(id);
+    this.setState({
+      movie,
+      loading: false,
+    });
+  }
+
+  render() {
+    const { movie, loading } = this.state;
+
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
+
+    if (loading) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-details">
@@ -18,9 +45,21 @@ class MovieDetails extends Component {
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <div>
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          <Link to="/">VOLTAR</Link>
+        </div>
       </div>
     );
   }
 }
 
+MovieDetails.propTypes = {
+  match: PropTypes.object,
+}.isRequired;
+
 export default MovieDetails;
+
+// Referências:
+// https://github.com/tryber/sd-010-a-project-movie-card-library-crud/pull/65/
+// https://github.com/tryber/sd-010-a-project-movie-card-library-crud/pull/47/

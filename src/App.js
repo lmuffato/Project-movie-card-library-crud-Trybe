35c841980a4d7a3ed178534541168dd220ac2623
Rodrigo Queiroz <rodrigoandradequeiroz@gmail.com>
2021-05-16 14:11:57 -0300

    req 1 fixed

--- src/App.js
+++ src/App.js
@@ -20,18 +20,12 @@ class App extends React.Component {
             <Route exact path="/" component={ MovieList } />
             <Route path="/movies/new" component={ NewMovie } />
             <Route
-              path="/movies/:id"
+              exact path="/movies/:id"
               render={ (props) => (<MovieDetails
                 { ...props }
               />) }
             />
-            <Route
-              exact
-              path="/movies/:id/edit"
-              render={ (props) => (<EditMovie
-                { ...props }
-              />) }
-            />
+            <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
             <Route component={ NotFound } />
           </Switch>
         </Router>

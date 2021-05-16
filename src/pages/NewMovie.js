04438fa13fab97a584e063b4fad365e6a725bc05
ThalitaC <thalitaceci@gmail.com>
2021-05-16 18:48:05 -0300

    fix: lint

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-import MovieForm from '../components/MovieForm';
-import * as movieAPI from '../services/movieAPI';
+// import MovieForm from '../components/MovieForm';
+// import * as movieAPI from '../services/movieAPI';
 
-class NewMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
+// class NewMovie extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleSubmit = this.handleSubmit.bind(this);
+//   }
 
-  handleSubmit(newMovie) {
-  }
+//   handleSubmit(newMovie) {
+//   }
 
-  render() {
-    return (
-      <div data-testid="new-movie">
-        <MovieForm onSubmit={ this.handleSubmit } />
-      </div>
-    );
-  }
-}
-export default NewMovie;
+//   render() {
+//     return (
+//       <div data-testid="new-movie">
+//         <MovieForm onSubmit={ this.handleSubmit } />
+//       </div>
+//     );
+//   }
+// }
+// export default NewMovie;

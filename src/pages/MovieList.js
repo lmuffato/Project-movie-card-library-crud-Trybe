13ec78336a0325c7d804088dbc1770ed8213c19b
Southier <patrick.southier@hotmail.com>
2021-05-17 02:42:34 -0300

    Completes requirement 2.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
 
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
+    this.makeMovieCards = this.makeMovieCards.bind(this);
+
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies = async () => {
+    this.setState(
+      { loading: true },
+      async () => {
+        const requestMovies = await movieAPI.getMovies();
+        this.setState({
+          movies: requestMovies,
+          loading: false,
+        });
+      },
+    );
+  }
+
+  makeMovieCards(param) {
+    return param.map((movie) => <MovieCard key={ movie.title } movie={ movie } />);
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
+    const loadingElement = <span>Carregando...</span>;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        { loading ? loadingElement : this.makeMovieCards(movies) }
       </div>
     );
   }

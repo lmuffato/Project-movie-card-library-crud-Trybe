43769503b21bc864b3dd6f2aa6ba6c32b29433a6
Caio Oliveira <parafrenteeadiante@gmail.com>
2021-05-19 12:48:23 -0300

    Corrigindo erro

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -10,13 +10,11 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={ (props) => <MovieList { ...props } /> } />
-        <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
-        <Route path="/movies/new" render={ (props) => <NewMovie { ...props } /> } />
-        <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
-        <Route path="/:any" component={ NotFound } />
-      </Switch>
+      <Route exact path="/" render={ (props) => <MovieList { ...props } /> } />
+      <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+      <Route path="/movies/new" render={ (props) => <NewMovie { ...props } /> } />
+      <Route path="/movies/:id/edit" render={ (props) => <EditMovie { ...props } /> } />
+      <Route path="/:any" component={ NotFound } />
     </BrowserRouter>
     // <div>Movie Card Library CRUD</div>
   );

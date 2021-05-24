ae5de6ca06334c7497b8b137445a417446472f31
carol-bitencourt <carolmedeirosbitencourt@gmail.com>
2021-05-24 01:14:14 -0300

    Requisito 2, ate aqui foi tranquilo, tao deixando a gente sonhar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -9,11 +9,13 @@ import NotFound from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path="/" component={MovieList} />
-      <Route path="/movies/:id" component={MovieDetails} />
-      <Route path="/movies/new" component={NewMovie} />
-      <Route path="/movies/:id/edit" component={EditMovie} />
-      <Route component={NotFound} />
+      <Switch>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" component={MovieDetails} />
+        <Route path="/movies/new" component={NewMovie} />
+        <Route path="/movies/:id/edit" component={EditMovie} />
+        <Route component={NotFound} />
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
-import * as movieAPI from '../services/movieAPI';
+import Loading from '../components/Loading';
+import { getMovies } from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
 
+    this.fetchMovies = this.fetchMovies.bind(this);
+
     this.state = {
       movies: [],
+      loading: true,
     };
   }
 
+  async fetchMovies() {
+    this.setState(
+      { loading: true },
+      async () => {
+        const requestMovies = await getMovies();
+
+        this.setState({
+          movies: requestMovies,
+          loading: false,
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.fetchMovies()
+  }
+
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
+    const loadingElement = <Loading/>
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {loading ? loadingElement : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

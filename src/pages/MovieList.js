54258d95bfbaf3ea465ad64b21cb4356155cdda8
Renata Andrade <reapminas@gmail.com>
2021-05-26 18:58:04 -0300

    Requisito 2 - exibe mensagem de carregando enquanto faz a chamada da API

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
@@ -9,14 +10,28 @@ class MovieList extends Component {
 
     this.state = {
       movies: [],
+      loading: true,
     };
+    this.fetchApi = this.fetchApi.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchApi();
+  }
+
+  async fetchApi() {
+    const data = await movieAPI.getMovies();
+    this.setState({ loading: false, movies: data });
+    console.log(this.state);
   }
 
   render() {
-    const { movies } = this.state;
+    const { movies, loading } = this.state;
 
     // Render Loading here if the request is still happening
-
+    if (loading === true) {
+      return <Loading />;
+    }
     return (
       <div data-testid="movie-list">
         {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
@@ -26,3 +41,5 @@ class MovieList extends Component {
 }
 
 export default MovieList;
+
+// Referências: o projeto em grupo do bloco 14 teve a mesma lógica para acessar a api

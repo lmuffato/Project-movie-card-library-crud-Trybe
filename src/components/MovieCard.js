889b37c5bf9ce60b6c16f5b11ba9721d8e306a85
carlos25xrm <carlos25xrm@gmail.com>
2021-05-26 20:10:23 -0300

    Tentando(2) comentar os arquivos para passar nos testes

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import MovieDetails from './pages/MovieDetails';
 
 class MovieCard extends React.Component {
   render() {
     return (
       <div data-testid="movie-card">
-        <h1>{this.props.}</h1>
+        <h1>{this.props}</h1>
       </div>
     );
   }

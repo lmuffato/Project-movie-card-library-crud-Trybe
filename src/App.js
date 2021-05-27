137e9984cf92fa709fe5ce9acc57a88c3ed71787
Luiz Otavio <luiz.otavio273@gmail.com>
2021-05-27 15:55:31 -0300

    Requisito 1 - Renderiza BrowserRouter no componente App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { Router } from 'react-router';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-      <div>
-        <h1>Movie Card Library CRUD</h1>
-        <h2>Iniciando o Projeto</h2>
-      </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/:id/edit" component={ EditMovie } />
+        <Route path="/*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 

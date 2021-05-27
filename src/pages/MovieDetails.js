513e510bb6a41eb14910d4ace9f1e6cd8b889c4a
Renata Andrade <reapminas@gmail.com>
2021-05-27 17:07:40 -0300

    Requisito 4 - criar conexao entre o link detalhes com informaçoes do filme

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,73 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movie: {},
+      loading: true,
+    };
+    this.fetchApi = this.fetchApi.bind(this);
+  }
+
+  componentDidMount() {
+    const { match } = this.props;
+    const { id } = match.params;
+    this.fetchApi(id);
+  }
+
+  async fetchApi() {
+    const { match } = this.props;
+    const { id } = match.params;
+    const data = await movieAPI.getMovie(id);
+    this.setState({ loading: false, movie: data });
+  }
+
   render() {
+    const { movie, loading } = this.state;
+    if (loading === true) {
+      return <Loading />;
+    }
     // Change the condition to check the state
     // if (true) return <Loading />;
 
-    const { storyline, imagePath, genre, rating, subtitle } = {};
+    const { id, title, storyline, imagePath, genre, rating, subtitle } = movie;
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <p>{ `Title: ${title}` }</p>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <div>
+          <button type="button">
+            <Link to="/">
+              VOLTAR
+            </Link>
+          </button>
+          <button type="button">
+            <Link to={ `/movies/${id}/edit` }>
+              EDITAR
+            </Link>
+          </button>
+        </div>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;

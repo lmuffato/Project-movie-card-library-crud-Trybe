5b528e8b4ce5f4fa5addf5ae82c68b7f0b249484
jlfagundes <jlfagundes@gmail.com>
2021-05-27 17:24:39 -0300

    feat: (req2) loading list movies

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
   constructor() {
     super();
-
+    // console.log('constructor');
     this.state = {
       movies: [],
     };
+
+    this.fetchMovies = this.fetchMovies.bind(this);
+  }
+
+  componentDidMount() {
+    // console.log('componetDidMount');
+    this.fetchMovies();
+  }
+
+  async fetchMovies() {
+    const listMovies = await movieAPI.getMovies();
+    this.setState({
+      movies: listMovies,
+    });
   }
 
   render() {
+    // console.log('render');
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {movies.length > 0 
+        ? movies.map((movie) => <MovieCard key={ movie.title } movie={ movie }/>)
+        : <Loading />
+        }
       </div>
     );
   }

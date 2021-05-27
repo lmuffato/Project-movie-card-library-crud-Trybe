337eb79bd7f4a4afcc4fdaed76a5b17e836829d3
akirakiraa <andretaki2@gmail.com>
2021-05-27 14:54:26 -0300

    Creating CRUD

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+    </BrowserRouter>
   );
 }
 

2861bc4da93b5df13c0445509c087b7e58a3b17d
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-27 15:43:28 -0300

    Requisito 01: Renderizar o BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
-// adicionando commit inicial
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
+
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <Router>
+      <div>Movie Card Library CRUD</div>
+      <Route exact path="/" component={ MovieList } />
+      <Route exact path="/movies/:id" component={ MovieDetails } />
+      <Route exact path="/movies/new" component={ NewMovie } />
+      <Route exact path="/movies/:id/edit" component={ EditMovie } />
+      <Route path="/*" component={ NotFound } />
+    </Router>
   );
 }
 

508cc90790c2d0816a38e0268a261cf4ef40e9de
Tiago Santos <tiago881@gmail.com>
2021-05-29 00:31:56 -0300

    5 - Realize uma requisição para buscar o filme que será editado em EditMovie

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
-import { MovieForm } from '../components';
+import { Loading, MovieForm } from '../components';
 import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      movie: {},
+      loading: false,
+      shouldRedirect: false,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit(updatedMovie) {
+    this.setState({ loading: true }, async () => {
+      await movieAPI.updateMovie(updatedMovie);
+      this.setState({
+        loading: false,
+        shouldRedirect: true,
+      });
+    });
   }
 
-  render() {
-    const { status, shouldRedirect, movie } = this.state;
-    if (shouldRedirect) {
-      // Redirect
-    }
+  fetchAPI = () => {
+    const { match: { params: { id } } } = this.props;
+    this.setState({ loading: true }, async () => {
+      const movie = await movieAPI.getMovie(id);
+      this.setState({
+        movie,
+        loading: false,
+      });
+    });
+  };
 
-    if (status === 'loading') {
-      // render Loading
-    }
+  componentDidMount = () => {
+    this.fetchAPI();
+  };
+
+  render() {
+    const { loading, shouldRedirect, movie } = this.state;
+    if (shouldRedirect) return <Redirect to="/" />;
+
+    if (loading) return <Loading />;
 
     return (
       <div data-testid="edit-movie">

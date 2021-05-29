0686ab530f0c3feac2b8f6d90d560cfb24da084e
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-29 10:04:11 -0300

    Resolvendo erros de arquivo

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -4,14 +4,40 @@ import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    state = {
+      movie: {},
+      loading: false,
+    };
+
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  async fetchMovie() {
+    this.setState({ loading: true });
+    const result = await movieAPI.getMovie();
+    this.setState({ movie: result });
+    this.setState({ loading: false });
+  }
+
   render() {
     // Change the condition to check the state
     // if (true) return <Loading />;
+    const { movie, loading } = this.state;
+    console.log(movie);
+
+    if (loading) { return <Loading />; }
 
     const { title, storyline, imagePath, genre, rating, subtitle } = {};
 
     return (
       <div data-testid="movie-details">
+        <p>{`Title: ${title}`}</p>
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>

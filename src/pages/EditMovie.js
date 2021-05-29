37dc0e7b86e4b48aef097c2ced2fda3cf4fef1ec
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-05-29 16:50:08 -0300

    requisito 07: linkar botoes

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,20 +1,40 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
+import Loading from '../components/Loading';
 import { MovieForm } from '../components';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class EditMovie extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      loading: true,
+    };
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchMovie = this.fetchMovie.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
   }
 
   handleSubmit(updatedMovie) {
+    movieAPI.updateMovie(updatedMovie);
+  }
+
+  async fetchMovie() {
+    const { match: { params: { id } } } = this.props;
+    const result = await movieAPI.getMovie(id);
+    this.setState({
+      movie: result,
+      loading: false,
+    });
   }
 
   render() {
-    const { status, shouldRedirect, movie } = this.state;
+    const { status, shouldRedirect, movie, loading } = this.state;
+    if (loading) { return <Loading />; }
     if (shouldRedirect) {
       // Redirect
     }
@@ -30,5 +50,13 @@ class EditMovie extends Component {
     );
   }
 }
+
+EditMovie.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }).isRequired,
+};
 
 export default EditMovie;

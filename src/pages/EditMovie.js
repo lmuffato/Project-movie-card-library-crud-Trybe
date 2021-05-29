50d4986c48c8668ea24a5e002ec2b0ddd2f0e1f7
Tiago Santos <tiago881@gmail.com>
2021-05-29 02:04:31 -0300

    import PropTypes

--- src/pages/EditMovie.js
+++ src/pages/EditMovie.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Redirect, Link } from 'react-router-dom';
 
 import { Loading, MovieForm } from '../components';

a33c4ffdb1f6e2ca50d58744264d53d4b176e6f0
Renata Andrade <reapminas@gmail.com>
2021-05-30 17:50:23 -0300

    Declara tipos Props MovieForm

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,16 +1,16 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
 class MovieForm extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = { ...props.movie };
+    this.state = { ...props.movie };
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit() {
-    // const { onSubmit } = this.props;
-    // onSubmit(this.state);
+    const { onSubmit } = this.props;
+    onSubmit(this.state);
   }
 
   updateMovie(field, newValue) {
@@ -39,7 +39,6 @@ class MovieForm extends React.Component {
 
   renderSubtitleInput() {
     const { subtitle } = this.state;
-
     return (
       <div>
         <label htmlFor="movie_subtitle">
@@ -163,5 +162,19 @@ class MovieForm extends React.Component {
     );
   }
 }
+
+MovieForm.propTypes = {
+  movie: PropTypes.shape({
+    bookmarked: PropTypes.bool,
+    genre: PropTypes.string,
+    id: PropTypes.number,
+    imagePath: PropTypes.string,
+    rating: PropTypes.number,
+    storyline: PropTypes.string,
+    subtitle: PropTypes.string,
+    title: PropTypes.string,
+  }).isRequired,
+  onSubmit: PropTypes.func.isRequired,
+};
 
 export default MovieForm;

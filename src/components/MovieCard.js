c595ae8c47ccb5ec5305df463902a66748da16e4
Luiz <luizfvaccarif@gmail.com>
2021-05-31 19:44:10 +0100

    Feat: requirement 3

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class MovieCard extends React.Component {
   render() {
+    const { movie: { title, storyline, id } } = this.props;
     return (
       <div data-testid="movie-card">
-        Movie Card
+        <p>{ title }</p>
+        <p>{ storyline }</p>
+        <Link to={ `movies/${id}` }>VER DETALHES</Link>
       </div>
     );
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.objectOf(PropTypes.object).isRequired,
+};
 
 export default MovieCard;

b262e84453fbd103997b326cce1822d512834c6f
pauloeliezerg <pauloeliezerg@gmail.com>
2021-05-31 01:58:35 -0300

    req 1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <div>Movie Card Library CRUD</div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <main>
+          <Switch>
+            <Route exact path="/" component={ MovieList } />
+            <Route exact path="/movies/:id/edit" component={ EditMovie } />
+            <Route exact path="/movies/new" component={ NewMovie } />
+            <Route exact path="/movies/:id" component={ MovieDetails } />
+            <Route path="" component={ NotFound } />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;

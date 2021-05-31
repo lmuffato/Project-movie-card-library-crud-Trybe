ca206adcb8be5e3913a6ed000e7a0ed4a5354e9a
Luiz Otavio <luiz.otavio273@gmail.com>
2021-05-31 15:31:48 -0300

    Requisito 2 - Fazer uma requisicao para buscar e mostrar filmes

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 import * as movieAPI from '../services/movieAPI';
 
@@ -12,14 +13,24 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    this.fetchMovies();
+  }
+
+  fetchMovies = async () => {
+    const { getMovies } = movieAPI;
+    const requestReturn = await getMovies();
+    this.setState({ movies: [...requestReturn] });
+  }
+
   render() {
     const { movies } = this.state;
 
-    // Render Loading here if the request is still happening
-
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {movies.length ? movies.map((movie) => (
+          <MovieCard key={ movie.title } movie={ movie } />))
+          : <Loading />}
       </div>
     );
   }

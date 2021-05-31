cf44f4bb577f2d04126e095a186bfd1e5bdc4c20
Apoc35 <alexv_silveira@outlook.com>
2021-05-31 10:35:50 -0300

    Envio dos requisitos fix.

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import { func } from 'prop-types';
 
 class MovieForm extends React.Component {
   constructor(props) {
@@ -163,5 +163,9 @@ class MovieForm extends React.Component {
     );
   }
 }
+
+MovieForm.propTypes = {
+  onSubmit: func,
+}.isRequired;
 
 export default MovieForm;

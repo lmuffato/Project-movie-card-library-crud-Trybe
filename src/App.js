57f7842f369b2c5ab4b2a193f61b78c432b7510f
Luiz <luizfvaccarif@gmail.com>
2021-05-31 15:16:14 +0100

    Feat: requirement 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MovieList from './pages/MovieList';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <Router>
+    <>
       <div>Movie Card Library CRUD</div>
-    </Router>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={ MovieList } />
+          <Route path="/movies/new" component={ NewMovie } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
+          <Route component={ NotFound } />
+        </Switch>
+      </Router>
+    </>
   );
 }
 
 export default App;
+
+// Código da rota NotFound baseada em:
+// https://ultimatecourses.com/blog/react-router-not-found-component

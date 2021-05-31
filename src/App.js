2e736a5c8ddfb20110dbfb82858451eb2bf98fa4
getuliogutemberg <getulio.dev@gmail.com>
2021-05-31 20:36:39 -0300

    adiciona rotas ao App

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
 import EditMovie from './pages/EditMovie';
 import NotFound from './pages/NotFound';
 
-function App() {
-  return (
-    <Router>
-      <div>Movie Card Library CRUD</div>
-      <Route exact path="/" component={ MovieList } />
-      <Route exact path="/movies/:id" component={ MovieDetails } />
-      <Route exact path="/movies/new" component={ NewMovie } />
-      <Route exact path="/movies/:id/edit" component={ EditMovie } />
-      <Route path="/*" component={ NotFound } />
-    </Router>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Movie Card Library CRUD</h1>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={ MovieList } />
+            <Route exact path="/movies/:id" component={ MovieDetails } />
+            <Route exact path="/movies/new" component={ NewMovie } />
+            <Route exact path="/movies/:id/edit" component={ EditMovie } />
+            <Route exact path="*" component={ NotFound } />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;

14e5715bc0a587509ac4f22b9d5e1242b718da0a
jlfagundes <jlfagundes@gmail.com>
2021-05-31 19:07:32 -0300

    lint

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard';
 import { Link } from 'react-router-dom';
+import MovieCard from '../components/MovieCard';
 import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 

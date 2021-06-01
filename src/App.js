6cadc73c46f5fadaca4224d1af20a1ccbacde2e9
Arlesson Moura <arlessonmss@gmail.com>
2021-06-01 03:10:06 -0300

    Feat: 1 requirement, need adjust

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import MovieList from './pages/MovieList';
+import NewMovie from './pages/NewMovie';
+import MovieDetails from './pages/MovieDetails';
+import EditMovie from './pages/EditMovie';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div>Movie Card Library CRUD</div>
+    <BrowserRouter>
+      <div>Movie Card Library CRUD</div>
+      <Switch>
+        <Route exact path="/" component={ MovieList } />
+        <Route path="/movies/:id" component={ MovieDetails } />
+        <Route exact path="/movies/new" component={ NewMovie } />
+        <Route exact path="movies/:id/edit" component={ EditMovie } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    </BrowserRouter>
   );
 }
 

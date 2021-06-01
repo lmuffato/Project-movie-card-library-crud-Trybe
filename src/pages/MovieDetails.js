0b67cce7dc819bfe1e365522139f0324e564ca91
¨WesleyFernandoAlmeida¨ <wesley.fer.almeida98@gmail.com>
2021-06-01 10:54:47 -0300

    Requisíto 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,25 +1,71 @@
 import React, { Component } from 'react';
-
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom'
 import * as movieAPI from '../services/movieAPI';
 import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      movie: {},
+      loading: false,
+    };
+  }
+
+  fetchAPI = () => {
+    const { match: { params: { id } } } = this.props;
+    this.setState({ loading: true }, async () => {
+      const movie = await movieAPI.getMovie(id);
+      this.setState({
+        movie,
+        loading: false,
+      });
+    });
+  };
+
+  componentDidMount = () => {
+    this.fetchAPI();
+  };
   render() {
-    // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { match: { params: { id } } } = this.props;
+    const { loading } = this.state;
+    if (loading) return <Loading />;
+
+    const { movie: {
+      title,
+      storyline,
+      imagePath,
+      genre,
+      rating,
+      subtitle,
+    } } = this.state;
 
     const { title, storyline, imagePath, genre, rating, subtitle } = {};
 
     return (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
+        <h3>{ `Title: ${title}` }</h3>
         <p>{ `Subtitle: ${subtitle}` }</p>
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+        <Link to="/">VOLTAR</Link>
+        <Link onClick={ async () => movieAPI.deleteMovie(id) } to="/">DELETAR</Link>
       </div>
     );
   }
 }
+
+MovieDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }).isRequired,
+};
 
 export default MovieDetails;

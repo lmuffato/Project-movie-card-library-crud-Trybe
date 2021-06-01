2156ad8db14b8f186704cbccc3e6d20d3f815789
akirakiraa <andretaki2@gmail.com>
2021-06-01 00:42:31 -0300

    forgot to save

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';// Renomear a BrowserRouter para Router , para nao ter confusão com com a proxima importação Route
-import { MovieDetails, MovieList, EditMovie, NewMovie, NotFound } from './pages';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';// Renomear a BrowserRouter para Router , para nao ter confusão com com a proxima importação Route
+import MovieList from './pages/MovieList';
+import NotFound from './pages/NotFound';
+import MovieDetails from './pages/MovieDetails';
+import NewMovie from './pages/NewMovie';
+import EditMovie from './pages/EditMovie';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Link to="/movies/new"> ADICIONAR CARTÃO </Link>
+    <div>
+      <div>Movie Card Library CRUD</div>
+      <Router>
         <Switch>
           <Route exact path="/" component={ MovieList } />
           <Route path="/movies/new" component={ NewMovie } />
-          <Route
-            path="/movies/:id/edit"
-            render={ (props) => <EditMovie { ...props } /> }
-          />
-          <Route path="/movies/:id" render={ (props) => <MovieDetails { ...props } /> } />
+          <Route path="/movies/:id/edit" component={ EditMovie } />
+          <Route path="/movies/:id" component={ MovieDetails } />
           <Route component={ NotFound } />
         </Switch>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 

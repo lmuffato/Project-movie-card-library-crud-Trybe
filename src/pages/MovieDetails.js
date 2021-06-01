da4d1e6107417ddba8b0b3cc81d88c195ea8af3a
biazidioti <bzferreira.bio@gmail.com>
2021-06-01 10:24:30 -0300

    Fix lint req 4

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,16 +1,41 @@
 import React, { Component } from 'react';
-
-// import * as movieAPI from '../services/movieAPI';
-// import { Loading } from '../components';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import * as movieAPI from '../services/movieAPI';
+import { Loading } from '../components';
 
 class MovieDetails extends Component {
+  constructor() {
+    super();
+    // const { match: { params: { id } } } = this.props;
+    this.state = {
+      movieId: null,
+      movie: {},
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovie();
+  }
+
+  fetchMovie() {
+    const { match: { params: { id } } } = this.props;
+    this.setState({ loading: true }, async () => {
+      const movie = await movieAPI.getMovie(id);
+      this.setState({
+        movieId: id,
+        loading: false,
+        movie });
+    });
+  }
+
   render() {
     // Change the condition to check the state
-    // if (true) return <Loading />;
+    const { movie, loading, movieId } = this.state;
+    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
 
-    const { title, storyline, imagePath, genre, rating, subtitle } = {};
-
-    return (
+    return !loading ? (
       <div data-testid="movie-details">
         <img alt="Movie Cover" src={ `../${imagePath}` } />
         <h2>{ `title: ${title}` }</h2>
@@ -18,9 +43,15 @@ class MovieDetails extends Component {
         <p>{ `Storyline: ${storyline}` }</p>
         <p>{ `Genre: ${genre}` }</p>
         <p>{ `Rating: ${rating}` }</p>
+        <Link to="/">VOLTAR</Link>
+        <Link to={ `/movies/${movieId}/edit` }>EDITAR</Link>
       </div>
-    );
+    ) : <Loading />;
   }
 }
+
+MovieDetails.propTypes = {
+  id: PropTypes.string,
+}.isRequired;
 
 export default MovieDetails;

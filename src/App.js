781b04d8fb16b1fc5d5ece1b1774478ce3abcc68
getuliogutemberg <getulio.dev@gmail.com>
2021-06-01 11:00:44 -0300

    organiza rotas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MovieList from './pages/MovieList';
 import MovieDetails from './pages/MovieDetails';
 import NewMovie from './pages/NewMovie';
@@ -9,15 +9,15 @@ import NotFound from './pages/NotFound';
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ MovieList } />
-          <Route exact path="/movies/:id" component={ MovieDetails } />
           <Route exact path="/movies/new" component={ NewMovie } />
           <Route exact path="/movies/:id/edit" component={ EditMovie } />
+          <Route exact path="/movies/:id" component={ MovieDetails } />
           <Route path="*" component={ NotFound } />
         </Switch>
-      </Router>
+      </BrowserRouter>
     );
   }
 }

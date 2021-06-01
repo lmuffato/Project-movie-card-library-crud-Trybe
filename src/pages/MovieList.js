26f29f1d213deb4fb474d97db1a0a5a84cab5769
Arlesson Moura <arlessonmss@gmail.com>
2021-06-01 05:10:15 -0300

    2 requiremente, error on handleState func.

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard';
-
+import Loading from '../components/Loading';
 import * as movieAPI from '../services/movieAPI';
 
 class MovieList extends Component {
@@ -12,14 +12,24 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    this.handleState();
+  }
+
+  handleState = () => {
+    const request = movieAPI.getMovies();
+    this.setState({ movies: [...request] });
+  }
+
   render() {
     const { movies } = this.state;
-
     // Render Loading here if the request is still happening
 
     return (
       <div data-testid="movie-list">
-        {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
+        {movies.length === 0
+          ? <Loading />
+          : movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
       </div>
     );
   }

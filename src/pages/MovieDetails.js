c89ccd646529d0a7b6caa988031a352ab2eb2ba4
getuliogutemberg <getulio.dev@gmail.com>
2021-05-31 23:25:00 -0300

    coloca links de voltar e editar

--- src/pages/MovieDetails.js
+++ src/pages/MovieDetails.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { Loading } from '../components';
 
 import * as movieAPI from '../services/movieAPI';
-import { Loading } from '../components';
 
 const { getMovie } = movieAPI;
 
@@ -34,21 +36,28 @@ export default class MovieDetails extends Component {
     // Change the condition to check the state
     // if (true) return <Loading />;
     const { movie } = this.state;
-    const { title, storyline, imagePath, genre, rating, subtitle } = movie;
+    const { title, storyline, imagePath, genre, rating, subtitle, id } = movie;
 
     if (!title) {
       return <Loading />;
     }
 
     return (
-      <div data-testid="movie-details">
-        <img alt="Movie Cover" src={ `../${imagePath}` } />
-        <h3>{ `Title: ${title}` }</h3>
-        <p>{ `Subtitle: ${subtitle}` }</p>
-        <p>{ `Storyline: ${storyline}` }</p>
-        <p>{ `Genre: ${genre}` }</p>
-        <p>{ `Rating: ${rating}` }</p>
+      <div>
+        <div data-testid="movie-details">
+          <img alt="Movie Cover" src={ `../${imagePath}` } />
+          <h3>{ `Title: ${title}` }</h3>
+          <p>{ `Subtitle: ${subtitle}` }</p>
+          <p>{ `Storyline: ${storyline}` }</p>
+          <p>{ `Genre: ${genre}` }</p>
+          <p>{ `Rating: ${rating}` }</p>
+        </div>
+        <div>
+          <Link to={ `/movies/${id}/edit` }>EDITAR</Link>
+          <Link to="/">VOLTAR</Link>
+        </div>
       </div>
+
     );
   }
 }

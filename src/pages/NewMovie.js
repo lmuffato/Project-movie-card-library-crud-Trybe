87412b731b5cd3ed83d04c9756f6efa0fbc6751d
biazidioti <bzferreira.bio@gmail.com>
2021-06-01 12:23:45 -0300

    add req 6

--- src/pages/NewMovie.js
+++ src/pages/NewMovie.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-
+import { Redirect } from 'react-router';
 import MovieForm from '../components/MovieForm';
-// import * as movieAPI from '../services/movieAPI';
+import * as movieAPI from '../services/movieAPI';
 
 class NewMovie extends Component {
-/*   constructor(props) {
+  constructor(props) {
     super(props);
+    this.state = { redirect: false };
     this.handleSubmit = this.handleSubmit.bind(this);
-  } */
+  }
 
-  /*   handleSubmit(newMovie) {
-  } */
+  handleSubmit(newMovie) {
+    movieAPI.createMovie(newMovie);
+    this.setState({ redirect: true });
+  }
 
   render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
+
     return (
       <div data-testid="new-movie">
         <MovieForm onSubmit={ this.handleSubmit } />

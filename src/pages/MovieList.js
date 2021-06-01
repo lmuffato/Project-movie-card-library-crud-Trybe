e341a3f06920ead8b9255256d3e852d4261cbd5c
getuliogutemberg <getulio.dev@gmail.com>
2021-05-31 21:05:28 -0300

    Solicita dados da Api para o MovieList

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Loading } from '../components';
 import MovieCard from '../components/MovieCard';
 
 import * as movieAPI from '../services/movieAPI';
+const { getMovies } = movieAPI;
 
 class MovieList extends Component {
   constructor() {
@@ -12,10 +14,28 @@ class MovieList extends Component {
     };
   }
 
+  componentDidMount() {
+    this.buildMovieList();
+  }
+  // --- solução encontrada por 'Paulo Eliezer' e adaptada ao contexto desse projeto
+  // faz a requisicao dos dados na API para preenchimento do state do componente.
+
+  async buildMovieList() {
+    this.setState({
+      movies: await getMovies(),
+    });
+  }
+
+  // ---fim do comentário.
+
   render() {
     const { movies } = this.state;
 
     // Render Loading here if the request is still happening
+
+    if (movies.length <= 0) {
+      return <Loading />;
+    }
 
     return (
       <div data-testid="movie-list">
